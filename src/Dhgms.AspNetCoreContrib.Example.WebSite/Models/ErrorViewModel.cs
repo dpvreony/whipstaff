cc38f5644f3650667072aae50fd1181c24014877
David Vreony <dpvreony@users.noreply.github.com>
2018-02-24 09:45:02 +0000

    added example website

---
+++ src/Dhgms.AspNetCoreContrib.Example.WebSite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Dhgms.AspNetCoreContrib.Example.WebSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
