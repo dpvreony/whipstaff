af72b0bfef98d9e76c9e922e171598b238853962
David Vreony <dpvreony@users.noreply.github.com>
2018-12-21 21:50:28 +0000

    exceptionless apm

---
+++ src/Dhgms.AspNetCoreContrib.Example.WebSite/Features/Apm/ApmApplicationStartHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dhgms.AspNetCoreContrib.Example.WebSite.Features.Apm.ApplicationInsights;
+using Dhgms.AspNetCoreContrib.Example.WebSite.Features.Apm.Exceptionless;
+using Exceptionless;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+
+namespace Dhgms.AspNetCoreContrib.Example.WebSite.Features.Apm
+{
+    public static class ApmApplicationStartHelper
+    {
+        public static void Configure(IConfiguration configuration, IApplicationBuilder app)
+        {
+            ExceptionlessApplicationStartHelper.Configure(configuration, app);
+            ApplicationInsightsApplicationStartHelper.Configure();
+        }
+    }
+}
---
+++ src/Dhgms.AspNetCoreContrib.Example.WebSite/Features/Apm/Exceptionless/ExceptionlessApplicationStartHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Exceptionless;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+
+namespace Dhgms.AspNetCoreContrib.Example.WebSite.Features.Apm.Exceptionless
+{
+    public static class ExceptionlessApplicationStartHelper
+    {
+        public static void Configure(IConfiguration configuration, IApplicationBuilder app)
+        {
+            var exceptionlessApiKey = configuration.GetValue<string>("APM_EXCEPTIONLESS_API_KEY", null);
+            if (!string.IsNullOrWhiteSpace(exceptionlessApiKey))
+            {
+                app.UseExceptionless();
+            }
+        }
+    }
+}
