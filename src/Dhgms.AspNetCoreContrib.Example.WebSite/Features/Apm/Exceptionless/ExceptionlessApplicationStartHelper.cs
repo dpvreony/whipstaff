9cfcdbb354dc6bd4c1a269d9e1e0ac9c230fab08
David Vreony <dpvreony@users.noreply.github.com>
2018-12-21 22:13:21 +0000

    nlog target setup

--- src/Dhgms.AspNetCoreContrib.Example.WebSite/Features/Apm/Exceptionless/ExceptionlessApplicationStartHelper.cs
+++ src/Dhgms.AspNetCoreContrib.Example.WebSite/Features/Apm/Exceptionless/ExceptionlessApplicationStartHelper.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Exceptionless;
+using Exceptionless.NLog;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 
@@ -16,7 +17,25 @@ namespace Dhgms.AspNetCoreContrib.Example.WebSite.Features.Apm.Exceptionless
             if (!string.IsNullOrWhiteSpace(exceptionlessApiKey))
             {
                 app.UseExceptionless();
+
+                ConfigureNLog(exceptionlessApiKey);
             }
+        }
+
+        private static void ConfigureNLog(string exceptionlessApiKey)
+        {
+            var nLogTarget = new global::Exceptionless.NLog.ExceptionlessTarget
+            {
+                Name = "Exceptionless",
+                ApiKey = exceptionlessApiKey,
+            };
+            nLogTarget.Fields.Add(new ExceptionlessField {Name = "host", Layout = "${machinename}"});
+            nLogTarget.Fields.Add(new ExceptionlessField {Name = "identity", Layout = "${identity}"});
+            nLogTarget.Fields.Add(new ExceptionlessField
+                {Name = "windows-identity", Layout = "${windows-identity:userName=True:domain=False}"});
+            nLogTarget.Fields.Add(new ExceptionlessField {Name = "process", Layout = "${processname}"});
+
+            NLog.LogManager.Configuration.AddTarget(nLogTarget);
         }
     }
 }
---
+++ src/Dhgms.AspNetCoreContrib.Example.WebSite/Features/Logging/LoggingApplicationStartHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+
+namespace Dhgms.AspNetCoreContrib.Example.WebSite.Features.Logging
+{
+    public static class LoggingApplicationStartHelper
+    {
+        public static void Configure(IConfiguration configuration, IApplicationBuilder app)
+        {
+
+        }
+    }
+}
