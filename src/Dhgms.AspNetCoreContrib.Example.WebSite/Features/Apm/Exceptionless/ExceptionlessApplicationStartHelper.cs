fdd0fac8f323b2be59ecf37bc5c597812ded16f0
David Vreony <dpvreony@users.noreply.github.com>
2018-12-23 22:27:59 +0000

    logging config

--- src/Dhgms.AspNetCoreContrib.Example.WebSite/Features/Apm/Exceptionless/ExceptionlessApplicationStartHelper.cs
+++ src/Dhgms.AspNetCoreContrib.Example.WebSite/Features/Apm/Exceptionless/ExceptionlessApplicationStartHelper.cs
@@ -18,24 +18,26 @@ namespace Dhgms.AspNetCoreContrib.Example.WebSite.Features.Apm.Exceptionless
             {
                 app.UseExceptionless();
 
-                ConfigureNLog(exceptionlessApiKey);
+                //ConfigureLogging(exceptionlessApiKey);
             }
         }
 
-        private static void ConfigureNLog(string exceptionlessApiKey)
-        {
-            var nLogTarget = new global::Exceptionless.NLog.ExceptionlessTarget
-            {
-                Name = "Exceptionless",
-                ApiKey = exceptionlessApiKey,
-            };
-            nLogTarget.Fields.Add(new ExceptionlessField {Name = "host", Layout = "${machinename}"});
-            nLogTarget.Fields.Add(new ExceptionlessField {Name = "identity", Layout = "${identity}"});
-            nLogTarget.Fields.Add(new ExceptionlessField
-                {Name = "windows-identity", Layout = "${windows-identity:userName=True:domain=False}"});
-            nLogTarget.Fields.Add(new ExceptionlessField {Name = "process", Layout = "${processname}"});
+        //private static void ConfigureNLog(string exceptionlessApiKey)
+        //{
+        //    global::Exceptionless.Extensions.Logging.ExceptionlessLogger
 
-            NLog.LogManager.Configuration.AddTarget(nLogTarget);
-        }
+        //    var nLogTarget = new global::Exceptionless.NLog.ExceptionlessTarget
+        //    {
+        //        Name = "Exceptionless",
+        //        ApiKey = exceptionlessApiKey,
+        //    };
+        //    nLogTarget.Fields.Add(new ExceptionlessField {Name = "host", Layout = "${machinename}"});
+        //    nLogTarget.Fields.Add(new ExceptionlessField {Name = "identity", Layout = "${identity}"});
+        //    nLogTarget.Fields.Add(new ExceptionlessField
+        //        {Name = "windows-identity", Layout = "${windows-identity:userName=True:domain=False}"});
+        //    nLogTarget.Fields.Add(new ExceptionlessField {Name = "process", Layout = "${processname}"});
+
+        //    NLog.LogManager.Configuration.AddTarget(nLogTarget);
+        //}
     }
 }
