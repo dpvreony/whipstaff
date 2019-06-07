eb031d6646da24ebacf28a2cf3af3d371728fcd0
David Vreony <dpvreony@users.noreply.github.com>
2019-06-07 18:57:41 +0100

    logic for excel workbook generation

---
+++ src/Dhgms.AspNetCoreContrib.Example.WebSite/Features/Excel/SpreadsheetDocumentHelper.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+
+namespace Dhgms.AspNetCoreContrib.Example.WebSite.Features.Excel
+{
+    public static class SpreadsheetDocumentHelper
+    {
+        public static SpreadsheetDocument GetWorkbookSpreadSheetDocument(
+            Stream stream,
+            IList<(string Name, Action<Sheet, WorksheetPart> Actor)> sheetActors)
+        {
+            var spreadsheetDocument = SpreadsheetDocument.Create(stream, SpreadsheetDocumentType.Workbook);
+
+            // Add a WorkbookPart to the document.
+            var workbookPart = spreadsheetDocument.AddWorkbookPart();
+            var workbook = new Workbook();
+            workbookPart.Workbook = workbook;
+
+            // Add a WorksheetPart to the WorkbookPart.
+            var worksheetPart = workbookPart.AddNewPart<WorksheetPart>();
+
+            var sheetData = new SheetData();
+            var worksheet = new Worksheet(sheetData);
+            worksheetPart.Worksheet = worksheet;
+
+            var sheets = workbook.AppendChild(new Sheets());
+
+            var workSheetPartId = workbookPart.GetIdOfPart(worksheetPart);
+
+            if (sheetActors?.Count > 0)
+            {
+                var sheetsToAdd = new List<Sheet>(sheetActors.Count);
+                foreach (var (name, actor) in sheetActors)
+                {
+                    var sheet = new Sheet
+                    {
+                        Id = workSheetPartId,
+                        SheetId = 1,
+                        Name = name,
+                    };
+
+                    actor(sheet, worksheetPart);
+                    sheetsToAdd.Add(sheet);
+                }
+
+                sheets.Append(sheetsToAdd);
+            }
+
+            return spreadsheetDocument;
+        }
+    }
+}
---
+++ src/Dhgms.AspNetCoreContrib.UnitTests/Features/SpreadsheetDocumentHelperTests.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Dhgms.AspNetCoreContrib.Example.WebSite.Features.Excel;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using Microsoft.Extensions.Logging;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Dhgms.AspNetCoreContrib.UnitTests.Features
+{
+    public static class SpreadsheetDocumentHelperTests
+    {
+        public sealed class GetWorkbookSpreadSheetDocumentMethod : Foundatio.Logging.Xunit.TestWithLoggingBase
+        {
+            public GetWorkbookSpreadSheetDocumentMethod(ITestOutputHelper output) : base(output)
+            {
+            }
+
+            [Fact]
+            public void ReturnsSpreadSheet()
+            {
+                var stream = new MemoryStream();
+                var sheetActors = new List<(string Name, Action<Sheet, WorksheetPart> Actor)>();
+                sheetActors.Add(("Sheet1", CreateSheet1));
+                var workbook = SpreadsheetDocumentHelper.GetWorkbookSpreadSheetDocument(stream, sheetActors);
+
+                Assert.NotNull(workbook);
+
+                workbook.Save();
+                workbook.Close();
+
+                var buffer = stream.ToArray();
+                var stringOutput = Encoding.UTF8.GetString(buffer);
+                this._logger.LogDebug(stringOutput);
+            }
+
+            private void CreateSheet1(Sheet sheet, WorksheetPart worksheetPart)
+            {
+                uint currentRow = 1;
+                var titleCell = InsertCellInWorksheet("A", currentRow, worksheetPart);
+                titleCell.CellValue = new CellValue("Title");
+                currentRow++;
+            }
+
+        private void WriteStringToCell(
+            string s,
+            uint currentRow,
+            WorksheetPart worksheetPart,
+            string value,
+            int columnSpan = 1,
+            int rowSpan = 1)
+        {
+            var cell = InsertCellInWorksheet("A", currentRow, worksheetPart);
+            cell.CellValue = new CellValue(value);
+        }
+
+        // Given a column name, a row index, and a WorksheetPart, inserts a cell into the worksheet. 
+        // If the cell already exists, returns it. 
+        private static Cell InsertCellInWorksheet(string columnName, uint rowIndex, WorksheetPart worksheetPart)
+        {
+            Worksheet worksheet = worksheetPart.Worksheet;
+            SheetData sheetData = worksheet.GetFirstChild<SheetData>();
+            string cellReference = columnName + rowIndex;
+
+            // If the worksheet does not contain a row with the specified row index, insert one.
+            var row = sheetData.Elements<Row>().FirstOrDefault(r => r.RowIndex == rowIndex);
+            if (row != null)
+            {
+                var cell = row.Elements<Cell>().FirstOrDefault(c => c.CellReference.Value == cellReference);
+                if (cell != null)
+                {
+                    return cell;
+                }
+            }
+            else
+            {
+                row = new Row { RowIndex = rowIndex };
+                sheetData.Append(row);
+            }
+
+            Cell refCell = null;
+            foreach (var cell in row.Elements<Cell>())
+            {
+                if (cell.CellReference.Value.Length == cellReference.Length)
+                {
+                    if (string.Compare(cell.CellReference.Value, cellReference, true) > 0)
+                    {
+                        refCell = cell;
+                        break;
+                    }
+                }
+            }
+
+            var newCell = new Cell { CellReference = cellReference };
+            row.InsertBefore(newCell, refCell);
+
+            worksheet.Save();
+            return newCell;
+        }
+        }
+    }
+}
