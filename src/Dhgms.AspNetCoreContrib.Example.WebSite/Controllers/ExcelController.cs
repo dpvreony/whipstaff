14052b0363c8dfab439a4daee0dd120f438afd0e
David Vreony <dpvreony@users.noreply.github.com>
2019-06-08 16:34:30 +0100

    skeleton excel controller example

---
+++ src/Dhgms.AspNetCoreContrib.Example.WebSite/Controllers/ExcelController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Dhgms.AspNetCoreContrib.Example.WebSite.Features.MediaTypeHeaders;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
+
+namespace Dhgms.AspNetCoreContrib.Example.WebSite.Controllers
+{
+    public class ExcelController : Controller
+    {
+        public async Task<IActionResult> GetAsync()
+        {
+            var filestream = Stream.Null;
+            if (filestream == null)
+            {
+                return this.NotFound();
+            }
+
+            var contentType = MediaTypeHeaderStringHelpers.ApplicationVndOpenXmlFormatsOfficeDocumentSpreadsheetMlSheet;
+            return this.File(filestream, contentType);
+        }
+    }
+}
---
+++ src/Dhgms.AspNetCoreContrib.Example.WebSite/Features/MediaTypeHeaders/MediaTypeHeaderStringHelpers.cs
@@ -0,0 +1,7 @@
+﻿namespace Dhgms.AspNetCoreContrib.Example.WebSite.Features.MediaTypeHeaders
+{
+    public static class MediaTypeHeaderStringHelpers
+    {
+        public static string ApplicationVndOpenXmlFormatsOfficeDocumentSpreadsheetMlSheet => "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    }
+}
