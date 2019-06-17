54c9318873da60ab55d0e52582e238a0a0242648
David Vreony <dvreony@sehnp.nhs.uk>
2019-06-17 18:07:25 +0100

    rate limiting middleware

---
+++ src/Dhgms.AspNetCoreContrib.Example.WebSite/Features/RequireForwardedForHeader/RequireForwardedForHeaderMiddleware.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dhgms.AspNetCoreContrib.Example.WebSite.Features.RequireForwardedForHeader
+{
+    public sealed class RequireForwardedForHeaderMiddleware
+    {
+    }
+}
