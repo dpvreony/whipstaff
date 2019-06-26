360232d5f2b26b94ead2a82d085b930951976edc
David Vreony <dpvreony@users.noreply.github.com>
2019-06-26 21:01:20 +0100

    added logging of responses in int test

--- src/Dhgms.AspNetCoreContrib.IntegrationTests/SecuredWebsiteTests.cs
+++ src/Dhgms.AspNetCoreContrib.IntegrationTests/SecuredWebsiteTests.cs
@@ -1,18 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Dhgms.AspNetCoreContrib.Example.WebSite;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Logging;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Dhgms.AspNetCoreContrib.IntegrationTests
 {
-    public sealed class SecuredWebsiteTests : IClassFixture<WebApplicationFactory<Startup>>
+    public sealed class SecuredWebsiteTests : Foundatio.Logging.Xunit.TestWithLoggingBase, IClassFixture<WebApplicationFactory<Startup>>
     {
         private readonly WebApplicationFactory<Startup> _factory;
 
-        public SecuredWebsiteTests(WebApplicationFactory<Startup> factory)
+        public SecuredWebsiteTests(ITestOutputHelper output, WebApplicationFactory<Startup> factory)
+            : base(output)
         {
             this._factory = factory;
         }
@@ -24,14 +28,16 @@ namespace Dhgms.AspNetCoreContrib.IntegrationTests
         [MemberData(nameof(GetReturnsSuccessAndCorrectContentTypetestSource))]
         public async Task GetReturnsSuccessAndCorrectContentType(string url)
         {
-            var client = _factory.CreateClient();
+            var client = this._factory.CreateClient();
 
-            var response = await client.GetAsync(url);
+            var response = await client.GetAsync(url).ConfigureAwait(false);
 
             response.EnsureSuccessStatusCode();
             Assert.Equal(
                 "text/html; charset=utf-8",
                 response.Content.Headers.ContentType.ToString());
+
+            await this.LogResponse(response).ConfigureAwait(false);
         }
 
         private static IEnumerable<object[]> GetGetReturnsSuccessAndCorrectContentTypetestSource()
@@ -43,6 +49,17 @@ namespace Dhgms.AspNetCoreContrib.IntegrationTests
                     "/",
                 },
             };
+        }
+
+        private async Task LogResponse(HttpResponseMessage response)
+        {
+            foreach (var httpResponseHeader in response.Headers)
+            {
+                this._logger.LogInformation($"{httpResponseHeader.Key}: {string.Join(",", httpResponseHeader.Value)}");
+            }
+
+            var result = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            this._logger.LogInformation(result);
         }
     }
 }
