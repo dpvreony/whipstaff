0357472334b8597a31063e31be5d96a3af25b841
David Vreony <dpvreony@users.noreply.github.com>
2019-10-20 10:22:39 +0100

    blazor starup tweaks

--- src/Dhgms.AspNetCoreContrib.Example.WebBlazorServer/Startup.cs
+++ src/Dhgms.AspNetCoreContrib.Example.WebBlazorServer/Startup.cs
@@ -2,6 +2,7 @@
 // This file is licensed to you under the MIT license.
 // See the LICENSE file in the project root for full license information.
 
+using System;
 using Dhgms.AspNetCoreContrib.Example.WebBlazorServer.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -11,22 +12,34 @@ using Microsoft.Extensions.Hosting;
 
 namespace Dhgms.AspNetCoreContrib.Example.WebBlazorServer
 {
-    public class Startup
+    /// <summary>
+    /// Represents startup logic for the server.
+    /// </summary>
+    public class Startup : IStartup
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Startup"/> class.
+        /// </summary>
+        /// <param name="configuration">Application configuration.</param>
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
+        /// <summary>
+        /// Gets the application configuration.
+        /// </summary>
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public void Configure(IApplicationBuilder app)
+        {
+            throw new NotImplementedException();
+        }
+
+        IServiceProvider ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            //services.AddSingleton<WeatherForecastService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +52,7 @@ namespace Dhgms.AspNetCoreContrib.Example.WebBlazorServer
             else
             {
                 app.UseExceptionHandler("/Error");
+
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
