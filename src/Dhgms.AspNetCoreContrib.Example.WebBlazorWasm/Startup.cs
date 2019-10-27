fced14922349beca00e0417b7f0cbca83ce1203a
David Vreony <dpvreony@users.noreply.github.com>
2019-10-27 01:18:23 +0100

    moved wasm back to ns2

--- src/Dhgms.AspNetCoreContrib.Example.WebBlazorWasm/Startup.cs
+++ src/Dhgms.AspNetCoreContrib.Example.WebBlazorWasm/Startup.cs
@@ -2,10 +2,7 @@
 // This file is licensed to you under the MIT license.
 // See the LICENSE file in the project root for full license information.
 
-using System;
-using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components.Builder;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Dhgms.AspNetCoreContrib.Example.WebBlazorWasm
@@ -13,28 +10,41 @@ namespace Dhgms.AspNetCoreContrib.Example.WebBlazorWasm
     /// <summary>
     /// Startup logic for Web Blazor WASM.
     /// </summary>
-    public class Startup : IStartup
+    public class Startup
     {
-        /// <inheritdoc/>
-        public void Configure(IApplicationBuilder app)
+        /// <summary>
+        /// Configures the application.
+        /// </summary>
+        /// <param name="app">Application Builder to configure against.</param>
+#pragma warning disable CA1822 // Mark members as static
+#pragma warning disable CC0091 // Use static method
+        public void Configure(IComponentsApplicationBuilder app)
+#pragma warning restore CC0091 // Use static method
+#pragma warning restore CA1822 // Mark members as static
         {
-            if (app == null)
-            {
-                throw new ArgumentNullException(nameof(app));
-            }
-
-            var componentsApplicationBuilder = app.ApplicationServices.GetService<IComponentsApplicationBuilder>();
 #pragma warning disable CC0021 // Use nameof
-            componentsApplicationBuilder.AddComponent<App>("app");
+            app.AddComponent<App>("app");
 #pragma warning restore CC0021 // Use nameof
         }
 
-        /// <inheritdoc/>
-        public IServiceProvider ConfigureServices(IServiceCollection services)
+        /// <summary>
+        /// Configure services for the application.
+        /// </summary>
+        /// <param name="services">Service collection to modify.</param>
+#pragma warning disable IDE0060 // Remove unused parameter
+#pragma warning disable CC0057 // Unused parameters
+#pragma warning disable CA1822 // Mark members as static
+#pragma warning disable CC0091 // Use static method
+#pragma warning disable RECS0154 // Parameter is never used
+#pragma warning disable CA1801 // Unused variable
+        public void ConfigureServices(IServiceCollection services)
+#pragma warning restore CA1801 // Unused variable
+#pragma warning restore RECS0154 // Parameter is never used
+#pragma warning restore CC0091 // Use static method
+#pragma warning restore CA1822 // Mark members as static
+#pragma warning restore CC0057 // Unused parameters
+#pragma warning restore IDE0060 // Remove unused parameter
         {
-#pragma warning disable ASP0000 // Do not call 'IServiceCollection.BuildServiceProvider' in 'ConfigureServices'
-            return services.BuildServiceProvider();
-#pragma warning restore ASP0000 // Do not call 'IServiceCollection.BuildServiceProvider' in 'ConfigureServices'
         }
     }
 }
