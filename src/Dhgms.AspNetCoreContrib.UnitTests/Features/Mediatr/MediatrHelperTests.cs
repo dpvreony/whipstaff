d8f30bd847c5ed6ca93217c7328524472b1c4df0
David Vreony <dpvreony@users.noreply.github.com>
2019-12-20 18:29:47 +0000

    mediatr config unit test

--- src/Dhgms.AspNetCoreContrib.UnitTests/Features/Mediatr/MediatrHelperTests.cs
+++ src/Dhgms.AspNetCoreContrib.UnitTests/Features/Mediatr/MediatrHelperTests.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.Claims;
+using System.Security.Principal;
 using System.Text;
+using System.Threading.Tasks;
 using Dhgms.AspNetCoreContrib.Fakes;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
@@ -31,14 +34,21 @@ namespace Dhgms.AspNetCoreContrib.UnitTests.Features.Mediatr
             /// Tests to ensure registration successfully takes place.
             /// </summary>
             [Fact]
-            public void ShouldSucceed()
+            public async Task ShouldSucceed()
             {
                 var services = new ServiceCollection();
                 Dhgms.AspNetCoreContrib.App.Features.Mediatr.MediatrHelpers.RegisterMediatrWithExplicitTypes(
                     services,
                     null,
-                    null,
+                    new MediatRServiceConfiguration(),
                     new FakeMediatrRegistration());
+
+                var serviceProvider = services.BuildServiceProvider();
+                var mediator = serviceProvider.GetService<IMediator>();
+                const int expected = 987654321;
+                var request = new FakeCrudAddCommand(expected, ClaimsPrincipal.Current);
+                var result = await mediator.Send(request).ConfigureAwait(false);
+                Assert.Equal(expected,result);
             }
 
         }
