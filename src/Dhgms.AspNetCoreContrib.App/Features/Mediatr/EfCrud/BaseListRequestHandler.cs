09104dc6b6f5694826e5f938bf7c92f5f269c46a
David Vreony <dpvreony@users.noreply.github.com>
2020-05-18 23:24:13 +0100

    logic for common ef+mediatr

---
+++ src/Dhgms.AspNetCoreContrib.App/Features/Mediatr/EfCrud/BaseListRequestHandler.cs
@@ -0,0 +1,59 @@
+﻿// Copyright (c) 2019 DHGMS Solutions and Contributors. All rights reserved.
+// This file is licensed to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dhgms.AspNetCoreContrib.App.Features.Mediatr.EfCrud
+{
+    public abstract class BaseListRequestHandler<TRequest, TResponse, TDbContext, TEntity, TKey> : IRequestHandler<TRequest, TResponse[]>
+        where TDbContext : DbContext
+        where TEntity : class
+        where TRequest : IRequest<TResponse>, IRequest<TResponse[]>
+    {
+        /// <inheritdoc />
+        public async Task<TResponse[]> Handle(TRequest request, CancellationToken cancellationToken)
+        {
+            using (var dbContext = GetDbContext())
+            {
+                var dbSet = GetDbSet(dbContext);
+                var query = dbSet.Where(GetWherePredicateExpression());
+
+                var orderingExpression = GetOrderingExpression();
+                query = GetWhetherOrderingIsByDescending()
+                    ? query.OrderByDescending(orderingExpression)
+                    : query.OrderBy(orderingExpression);
+
+                var skipCount = 0;
+                query = query.Skip(skipCount);
+
+                var takeCount = 1;
+                query = query.Take(takeCount);
+
+                var result = await query.Select(GetSelectorExpression())
+                    .ToArrayAsync(cancellationToken)
+                    .ConfigureAwait(false);
+
+                return result;
+            }
+        }
+
+        protected abstract bool GetWhetherOrderingIsByDescending();
+
+        protected abstract Expression<Func<TEntity, TKey>> GetOrderingExpression();
+
+        protected abstract Expression<Func<TEntity, int, TResponse>> GetSelectorExpression();
+
+        protected abstract Expression<Func<TEntity, int, bool>> GetWherePredicateExpression();
+
+        protected abstract TDbContext GetDbContext();
+
+        protected abstract DbSet<TEntity> GetDbSet(TDbContext dbContext);
+    }
+}
---
+++ src/Dhgms.AspNetCoreContrib.App/Features/Mediatr/EfCrud/BaseViewRequestHandler.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) 2019 DHGMS Solutions and Contributors. All rights reserved.
+// This file is licensed to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dhgms.AspNetCoreContrib.App.Features.Mediatr.EfCrud
+{
+    /// <summary>
+    /// Base class for fetching a single record from mediatr.
+    /// </summary>
+    /// <typeparam name="TRequest">The request type for the MediatR query.</typeparam>
+    /// <typeparam name="TResponse">The response type coming out of MediatR.</typeparam>
+    /// <typeparam name="TDbContext">The type for the Entity Framework DB Context.</typeparam>
+    /// <typeparam name="TEntity">The type for the Entity Framework DB Set.</typeparam>
+    public abstract class BaseViewRequestHandler<TRequest, TResponse, TDbContext, TEntity> : IRequestHandler<TRequest, TResponse>
+        where TDbContext : DbContext
+        where TEntity : class
+        where TRequest : IRequest<TResponse>
+    {
+        /// <inheritdoc />
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken)
+        {
+            using (var dbContext = GetDbContext())
+            {
+                var dbSet = GetDbSet(dbContext);
+                var result = await dbSet.Where(GetWherePredicateExpression())
+                    .Select(GetSelectorExpression())
+                    .FirstOrDefaultAsync(cancellationToken)
+                    .ConfigureAwait(false);
+
+                return result;
+            }
+        }
+
+        /// <summary>
+        /// Get the expression used for selecting records.
+        /// </summary>
+        /// <returns></returns>
+        protected abstract Expression<Func<TEntity, int, TResponse>> GetSelectorExpression();
+
+        protected abstract Expression<Func<TEntity, int, bool>> GetWherePredicateExpression();
+
+        protected abstract TDbContext GetDbContext();
+
+        protected abstract DbSet<TEntity> GetDbSet(TDbContext dbContext);
+    }
+}
