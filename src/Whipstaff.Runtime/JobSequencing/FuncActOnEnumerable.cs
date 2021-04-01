24eaf442edcc902214ae1d8971d4c00f2d70691e
David Vreony <dpvreony@users.noreply.github.com>
2021-04-01 16:06:46 +0100

    job sequencing

---
+++ src/Whipstaff.Runtime/JobSequencing/FuncActOnEnumerable.cs
@@ -0,0 +1,63 @@
+﻿// Copyright (c) 2020 DHGMS Solutions and Contributors. All rights reserved.
+// DHGMS Solutions and Contributors licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Whipstaff.Runtime.JobSequencing
+{
+    /// <summary>
+    /// Job Sequence helper for having a func work on an enumerable.
+    /// </summary>
+    /// <typeparam name="T">The type for the POCO object.</typeparam>
+    public sealed class FuncActOnEnumerable<T> : ActOnEnumerable<T>
+    {
+        private readonly Func<T, Task> _actOnItemAsyncFunc;
+        private readonly Func<Task> _onNoItemsHandledAsync;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FuncActOnEnumerable{T}"/> class.
+        /// </summary>
+        /// <param name="actOnItemAsyncFunc">Function to execute on each item in the collection.</param>
+        /// <param name="onNoItemsHandledAsync">Function to execute if the collection is empty.</param>
+        public FuncActOnEnumerable(
+            Func<T, Task> actOnItemAsyncFunc,
+            Func<Task> onNoItemsHandledAsync)
+        {
+            _actOnItemAsyncFunc =
+                actOnItemAsyncFunc ?? throw new ArgumentNullException(nameof(actOnItemAsyncFunc));
+            _onNoItemsHandledAsync =
+                onNoItemsHandledAsync ?? throw new ArgumentNullException(nameof(onNoItemsHandledAsync));
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FuncActOnEnumerable{T}"/> class.
+        /// </summary>
+        /// <param name="enumerable">Enumerable object to iterate through.</param>
+        /// <param name="actOnItemAsyncFunc">Function to execute on each item in the collection.</param>
+        /// <param name="onNoItemsHandledAsync">Function to execute if the collection is empty.</param>
+        /// <returns>A <see cref="Task"/> representing the result of the asynchronous operation.</returns>
+        public static Task ActOnCollectionAsync(
+            IEnumerable<T> enumerable,
+            Func<T, Task> actOnItemAsyncFunc,
+            Func<Task> onNoItemsHandledAsync)
+        {
+            var instance = new FuncActOnEnumerable<T>(actOnItemAsyncFunc, onNoItemsHandledAsync);
+            return instance.ActOnCollectionAsync(enumerable);
+        }
+
+        /// <inheritdoc/>
+        protected override Task ActOnItemAsync(T item)
+        {
+            return _actOnItemAsyncFunc(item);
+        }
+
+        /// <inheritdoc/>
+        protected override Task OnNoItemsHandledAsync()
+        {
+            return _onNoItemsHandledAsync();
+        }
+    }
+}
