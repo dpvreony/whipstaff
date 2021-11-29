948adf1f505bc056015b0bbefb27c6601436027e
David Vreony <dpvreony@users.noreply.github.com>
2021-11-29 21:41:42 +0000

    Update StringExtensions.cs

--- src/Whipstaff.Runtime/Extensions/StringExtensions.cs
+++ src/Whipstaff.Runtime/Extensions/StringExtensions.cs
@@ -3,8 +3,6 @@
 // See the LICENSE file in the project root for full license information.
 
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Dhgms.NetContrib.UnitTests.Features.Extensions
 {
