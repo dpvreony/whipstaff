ee743f89482250adbd960e13338446c3d0f85d37
David Vreony <dpvreony@users.noreply.github.com>
2021-12-11 09:11:35 +0000

    enable nrt and code analysis

--- src/Whipstaff.AspNetCore/Features/RequireForwardedForHeader/WhipcordHttpStatusCode.cs
+++ src/Whipstaff.AspNetCore/Features/RequireForwardedForHeader/WhipcordHttpStatusCode.cs
@@ -5,29 +5,29 @@
 namespace Whipstaff.AspNetCore.Features.RequireForwardedForHeader
 {
     /// <summary>
-    /// HTTP Status Codes that are specific to Whipcord.
+    /// HTTP Status Codes that are specific to Whipstaff.
     /// </summary>
-    public enum WhipcordHttpStatusCode
+    public static class WhipcordHttpStatusCode
     {
         /// <summary>
         /// Server side error for when an X-Forwarded-For header is required but not
         /// received. It's a 5xx error as the client wouldn't be aware they should
         /// be going through a load balancer.
         /// </summary>
-        ExpectedXForwardedFor = 599,
+        public const int ExpectedXForwardedFor = 599;
 
         /// <summary>
         /// Server side error for when an X-Forwarded-Proto header is required but not
         /// received. It's a 5xx error as the client wouldn't be aware they should
         /// be going through a load balancer.
         /// </summary>
-        ExpectedXForwardedProto = 598,
+        public const int ExpectedXForwardedProto = 598;
 
         /// <summary>
         /// Server side error for when an X-Forwarded-Host header is required but not
         /// received. It's a 5xx error as the client wouldn't be aware they should
         /// be going through a load balancer.
         /// </summary>
-        ExpectedXForwardedHost = 597,
+        public const int ExpectedXForwardedHost = 597;
     }
 }
