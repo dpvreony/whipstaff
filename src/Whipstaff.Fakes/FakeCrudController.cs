bbc24acf18c17802e5812272121503040392f8db
David Vreony <dpvreony@users.noreply.github.com>
2022-01-08 13:08:40 +0000

    restructure log message actions into a poco

--- src/Whipstaff.Fakes/FakeCrudController.cs
+++ src/Whipstaff.Fakes/FakeCrudController.cs
@@ -38,7 +38,8 @@ namespace Whipstaff.Testing
         long?,
         FakeCrudUpdateCommand,
         int,
-        FakeCrudUpdateResponse>
+        FakeCrudUpdateResponse,
+        FakeCrudControllerLogMessageActions>
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="FakeCrudController"/> class.
@@ -48,6 +49,7 @@ namespace Whipstaff.Testing
         /// <param name="mediator">The mediatr object to publish CQRS messages to.</param>
         /// <param name="commandFactory"></param>
         /// <param name="queryFactory"></param>
+        /// <param name="logMessageActions"></param>
         public FakeCrudController(IAuthorizationService authorizationService,
             ILogger<FakeCrudController> logger,
             IMediator mediator,
@@ -65,18 +67,15 @@ namespace Whipstaff.Testing
                 FakeCrudListRequest,
                 IList<int>,
                 FakeCrudViewQuery,
-                FakeCrudViewResponse> queryFactory)
+                FakeCrudViewResponse> queryFactory,
+            FakeCrudControllerLogMessageActions logMessageActions)
             : base(
                 authorizationService,
                 logger,
                 mediator,
                 commandFactory,
                 queryFactory,
-                LoggerMessage.Define<string>(LogLevel.Debug, new EventId(1), "{Message}"),
-                LoggerMessage.Define<string>(LogLevel.Debug, new EventId(2), "{Message}"),
-                LoggerMessage.Define<string>(LogLevel.Debug, new EventId(3), "{Message}"),
-                LoggerMessage.Define<string>(LogLevel.Debug, new EventId(4), "{Message}"),
-                LoggerMessage.Define<string>(LogLevel.Debug, new EventId(5), "{Message}"))
+                logMessageActions)
         {
         }
 
---
+++ src/Whipstaff.Fakes/FakeCrudControllerLogMessageActions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using Whipstaff.AspNetCore.Features.Logging;
+using Whipstaff.Core.Logging;
+
+namespace Whipstaff.Testing
+{
+    /// <summary>
+    /// Log Message Actions for the Fake CRUD controller.
+    /// </summary>
+    public sealed class FakeCrudControllerLogMessageActions : ICrudControllerLogMessageActions
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FakeCrudControllerLogMessageActions"/> class.
+        /// </summary>
+        public FakeCrudControllerLogMessageActions()
+        {
+            AddEventLogMessageAction = LoggerMessageFactory.GetDebugBasicLoggerMessageActionForEventId(new EventId(1));
+            DeleteEventLogMessageAction = LoggerMessageFactory.GetDebugBasicLoggerMessageActionForEventId(new EventId(2));
+            ListEventLogMessageAction = LoggerMessageFactory.GetDebugBasicLoggerMessageActionForEventId(new EventId(3));
+            UpdateEventLogMessageAction = LoggerMessageFactory.GetDebugBasicLoggerMessageActionForEventId(new EventId(4));
+            ViewEventLogMessageAction = LoggerMessageFactory.GetDebugBasicLoggerMessageActionForEventId(new EventId(5));
+        }
+
+        /// <inheritdoc />
+        public Action<ILogger, string, Exception?> ListEventLogMessageAction { get; init; }
+
+        /// <inheritdoc />
+        public Action<ILogger, string, Exception?> ViewEventLogMessageAction { get; init; }
+
+        /// <inheritdoc />
+        public Action<ILogger, string, Exception?> AddEventLogMessageAction { get; init; }
+
+        /// <inheritdoc />
+        public Action<ILogger, string, Exception?> DeleteEventLogMessageAction { get; init; }
+
+        /// <inheritdoc />
+        public Action<ILogger, string, Exception?> UpdateEventLogMessageAction { get; init; }
+    }
+}
