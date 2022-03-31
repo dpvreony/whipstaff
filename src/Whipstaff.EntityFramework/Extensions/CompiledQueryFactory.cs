2913816e9bfcbf6c7913da8fdf246dd1abd07e10
David Vreony <dpvreony@users.noreply.github.com>
2022-03-31 22:28:23 +0100

    Update CompiledQueryFactory.cs

--- src/Whipstaff.EntityFramework/Extensions/CompiledQueryFactory.cs
+++ src/Whipstaff.EntityFramework/Extensions/CompiledQueryFactory.cs
@@ -28,6 +28,20 @@ namespace Whipstaff.EntityFramework.Extensions
         }
 
         /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TDbContext"></typeparam>
+        /// <typeparam name="TDbSet"></typeparam>
+        /// <param name="dbSetSelector"></param>
+        /// <returns></returns>
+        public static Func<TDbContext, int, IEnumerable<TDbSet>> GetWhereUniqueIntIdEqualsCompiledQuery<TDbContext, TDbSet>(System.Linq.Expressions.Expression<Func<TDbContext, DbSet<TDbSet>>> dbSetSelector)
+            where TDbContext : DbContext
+            where TDbSet : class, IIntId
+        {
+            return EF.CompileQuery((TDbContext context, int id) => dbSetSelector.Compile()(context).Where(entity => entity.Id == id));
+        }
+
+        /// <summary>
         /// Gets a compiled query for retrieving a record with a long primary key.
         /// </summary>
         /// <typeparam name="TDbContext">The type for the database context.</typeparam>
@@ -40,5 +54,18 @@ namespace Whipstaff.EntityFramework.Extensions
         {
             return EF.CompileAsyncQuery((TDbContext context, long id) => dbSetSelector.Compile()(context).Where(entity => entity.Id == id));
         }
-    }
+
+        /// <summary>
+        /// Gets a compiled query for retrieving a record with a long primary key.
+        /// </summary>
+        /// <typeparam name="TDbContext">The type for the database context.</typeparam>
+        /// <typeparam name="TDbSet">The type for the Database set.</typeparam>
+        /// <param name="dbSetSelector">Function to select the DBSet used for the compiled query</param>
+        /// <returns></returns>
+        public static Func<TDbContext, long, IEnumerable<TDbSet>> GetWhereUniqueLongIdEqualsCompiledQuery<TDbContext, TDbSet>(System.Linq.Expressions.Expression<Func<TDbContext, DbSet<TDbSet>>> dbSetSelector)
+            where TDbContext : DbContext
+            where TDbSet : class, ILongId
+        {
+            return EF.CompileQuery((TDbContext context, long id) => dbSetSelector.Compile()(context).Where(entity => entity.Id == id));
+        }    }
 }
