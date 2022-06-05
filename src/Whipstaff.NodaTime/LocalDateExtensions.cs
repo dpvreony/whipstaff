b7b09dc7af2af826a0d1962031517c129658b0d9
David Vreony <dpvreony@users.noreply.github.com>
2022-06-05 19:21:20 +0100

    localdate extensions from personal site

---
+++ src/Whipstaff.NodaTime/LocalDateExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+
+namespace Whipstaff.NodaTime
+{
+    public static class LocalDateExtensions
+    {
+        public static string ToYearMonthDayString(this global::NodaTime.LocalDate instance)
+        {
+            return instance.ToString("yyyy-MM-dd", DateTimeFormatInfo.InvariantInfo);
+        }
+    }
+}
