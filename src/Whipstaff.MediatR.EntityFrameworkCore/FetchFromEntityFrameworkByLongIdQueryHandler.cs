c8b6b95beeacde3551c81a7a99d246c038f26d16
David Vreony <dpvreony@users.noreply.github.com>
2022-08-19 18:32:17 +0100

    refactor of some cqrs actions

--- src/Whipstaff.MediatR.EntityFrameworkCore/FetchFromEntityFrameworkByLongIdQueryHandler.cs
+++ src/Whipstaff.MediatR.EntityFrameworkCore/FetchFromEntityFrameworkByLongIdQueryHandler.cs
@@ -7,36 +7,36 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Threading;
 using System.Threading.Tasks;
-using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Whipstaff.Core.Entities;
+using Whipstaff.Core.Mediatr;
 
 namespace Whipstaff.MediatR.EntityFrameworkCore
 {
     /// <summary>
     /// MediatR request handler for acting on an entity framework dbset.
     /// </summary>
-    /// <typeparam name="TRequest">The type for the MediatR Request.</typeparam>
+    /// <typeparam name="TQuery">The type for the MediatR Query.</typeparam>
     /// <typeparam name="TDbContext">The type for the Entity Framework DB Context.</typeparam>
     /// <typeparam name="TEntity">The type for the POCO object.</typeparam>
     /// <typeparam name="TResult">The type for the Result.</typeparam>
-    public abstract class FetchFromEntityFrameworkByInt32IdRequestHandler<TRequest, TDbContext, TEntity, TResult>
-        : FetchFromEntityFrameworkRequestHandler<TRequest, TDbContext, TEntity, TResult>
+    public abstract class FetchFromEntityFrameworkByLongIdQueryHandler<TQuery, TDbContext, TEntity, TResult>
+        : FetchFromEntityFrameworkQueryHandler<TQuery, TDbContext, TEntity, TResult>
         where TDbContext : DbContext
-        where TRequest : IRequest<TResult>, IIntId
-        where TEntity : class, IIntId
+        where TQuery : IQuery<TResult>, ILongId
+        where TEntity : class, ILongId
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="FetchFromEntityFrameworkByInt32IdRequestHandler{TRequest, TDbContext, TEntity,TResult}"/> class.
+        /// Initializes a new instance of the <see cref="FetchFromEntityFrameworkByLongIdQueryHandler{TQuery, TDbContext, TEntity,TResult}"/> class.
         /// </summary>
         /// <param name="dbContextFactory">The factory for the database context.</param>
-        protected FetchFromEntityFrameworkByInt32IdRequestHandler(Func<Task<TDbContext>> dbContextFactory)
+        protected FetchFromEntityFrameworkByLongIdQueryHandler(Func<Task<TDbContext>> dbContextFactory)
             : base(dbContextFactory)
         {
         }
 
         /// <inheritdoc/>
-        protected sealed override Expression<Func<TEntity, bool>> GetWherePredicate(TRequest request)
+        protected sealed override Expression<Func<TEntity, bool>> GetWherePredicate(TQuery request)
         {
             return entity => entity.Id == request.Id;
         }
--- src/Whipstaff.MediatR.EntityFrameworkCore/FetchFromEntityFrameworkQueryHandler{TQuery,TDbContext,TEntity,TResult}.cs
+++ src/Whipstaff.MediatR.EntityFrameworkCore/FetchFromEntityFrameworkQueryHandler{TQuery,TDbContext,TEntity,TResult}.cs
@@ -7,36 +7,36 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Threading;
 using System.Threading.Tasks;
-using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Whipstaff.Core.Mediatr;
 
 namespace Whipstaff.MediatR.EntityFrameworkCore
 {
     /// <summary>
     /// MediatR request handler for acting on an entity framework dbset.
     /// </summary>
-    /// <typeparam name="TRequest">The type for the MediatR Request.</typeparam>
+    /// <typeparam name="TQuery">The type for the MediatR Query.</typeparam>
     /// <typeparam name="TDbContext">The type for the Entity Framework DB Context.</typeparam>
     /// <typeparam name="TEntity">The type for the POCO object.</typeparam>
     /// <typeparam name="TResult">The type for the Result.</typeparam>
-    public abstract class FetchFromEntityFrameworkRequestHandler<TRequest, TDbContext, TEntity, TResult> : IRequestHandler<TRequest, TResult?>
+    public abstract class FetchFromEntityFrameworkQueryHandler<TQuery, TDbContext, TEntity, TResult> : IQueryHandler<TQuery, TResult?>
         where TDbContext : DbContext
-        where TRequest : IRequest<TResult?>
+        where TQuery : IQuery<TResult?>
         where TEntity : class
     {
         private readonly Func<Task<TDbContext>> _dbContextFactory;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="FetchFromEntityFrameworkRequestHandler{TRequest, TDbContext, TEntity,TResult}"/> class.
+        /// Initializes a new instance of the <see cref="FetchFromEntityFrameworkQueryHandler{TRequest, TDbContext, TEntity,TResult}"/> class.
         /// </summary>
         /// <param name="dbContextFactory">The factory for the database context.</param>
-        protected FetchFromEntityFrameworkRequestHandler(Func<Task<TDbContext>> dbContextFactory)
+        protected FetchFromEntityFrameworkQueryHandler(Func<Task<TDbContext>> dbContextFactory)
         {
             _dbContextFactory = dbContextFactory ?? throw new ArgumentNullException(nameof(dbContextFactory));
         }
 
         /// <inheritdoc/>
-        public async Task<TResult?> Handle(TRequest request, CancellationToken cancellationToken)
+        public async Task<TResult?> Handle(TQuery request, CancellationToken cancellationToken)
         {
             using (var dbContext = await _dbContextFactory().ConfigureAwait(false))
             {
@@ -56,7 +56,7 @@ namespace Whipstaff.MediatR.EntityFrameworkCore
         /// </summary>
         /// <param name="request">MediatR request.</param>
         /// <returns>Where predicate for the query.</returns>
-        protected abstract Expression<Func<TEntity, bool>> GetWherePredicate(TRequest request);
+        protected abstract Expression<Func<TEntity, bool>> GetWherePredicate(TQuery request);
 
         /// <summary>
         /// Gets the Selector for the result output from the query.
