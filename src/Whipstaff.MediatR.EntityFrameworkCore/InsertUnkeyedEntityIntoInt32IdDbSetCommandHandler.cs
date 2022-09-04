5c1381614d00ac4e7186f37e1732e639f9fa1dcf
David Vreony <dpvreony@users.noreply.github.com>
2022-09-04 17:29:17 +0100

    more prep for ef commands

--- src/Whipstaff.MediatR.EntityFrameworkCore/InsertUnkeyedEntityIntoInt32IdDbSetCommandHandler.cs
+++ src/Whipstaff.MediatR.EntityFrameworkCore/InsertUnkeyedEntityIntoInt32IdDbSetCommandHandler.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Copyright (c) 2022 DHGMS Solutions and Contributors. All rights reserved.
+// This file is licensed to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
 
 namespace Whipstaff.MediatR.EntityFrameworkCore
 {
-    internal class InsertUnkeyedEntityIntoInt32IdDbSetCommandHandler
+    public abstract class InsertUnkeyedEntityIntoInt32IdDbSetCommandHandler<TCommand, TResponse> : InsertUnkeyedEntityIntoKeyedDbSetCommandHandler<TCommand, TResponse>
     {
     }
 }
--- src/Whipstaff.MediatR.EntityFrameworkCore/InsertUnkeyedEntityIntoKeyedDbSetCommandHandler.cs
+++ src/Whipstaff.MediatR.EntityFrameworkCore/InsertUnkeyedEntityIntoKeyedDbSetCommandHandler.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿// Copyright (c) 2022 DHGMS Solutions and Contributors. All rights reserved.
+// This file is licensed to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System.Threading;
 using System.Threading.Tasks;
+using Whipstaff.Core.Mediatr;
 
 namespace Whipstaff.MediatR.EntityFrameworkCore
 {
-    internal class InsertUnkeyedEntityIntoKeyedDbSetCommandHandler
+    public abstract class InsertUnkeyedEntityIntoKeyedDbSetCommandHandler<TCommand, TResponse> : ICommandHandler<TCommand, TResponse>
     {
+        /// <inheritdoc/>
+        public Task<TResponse> Handle(TCommand request, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
