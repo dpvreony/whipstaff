156d01a0dada4581701c9cc30947df49151e0ca4
David Vreony <dpvreony@users.noreply.github.com>
2022-10-26 22:01:03 +0100

    add x509 store helpers

---
+++ src/Whipstaff.Runtime/Cryptography/X509/X509StoreHelpers.cs
@@ -0,0 +1,71 @@
+﻿// Copyright (c) 2022 DHGMS Solutions and Contributors. All rights reserved.
+// This file is licensed to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Whipstaff.Runtime.Cryptography.X509
+{
+    /// <summary>
+    /// Helpers for <see cref="X509Store"/>.
+    /// </summary>
+    public static class X509StoreHelpers
+    {
+        /// <summary>
+        /// Gets a collection of expired certificates.
+        /// </summary>
+        /// <param name="store">The X509 store.</param>
+        /// <returns>Expired certificates.</returns>
+        public static IEnumerable<X509Certificate2> GetExpiredCertificateCollection(X509Store store)
+        {
+            var cutOff = DateTime.UtcNow;
+
+            return GetExpiringCertificateCollection(store, cutOff);
+        }
+
+        /// <summary>
+        /// Gets a collection of expiring certificates.
+        /// </summary>
+        /// <param name="store">The X509 store.</param>
+        /// <param name="cutOff">The timestamp to use as the cut off.</param>
+        /// <returns>Expiring certificates.</returns>
+        public static IEnumerable<X509Certificate2> GetExpiringCertificateCollection(X509Store store, DateTime cutOff)
+        {
+            return GetCertificateCollectionViaSelector(
+                store,
+                certificate => certificate.NotAfter > cutOff);
+        }
+
+        /// <summary>
+        /// Gets a collection of certificates based on a selector.
+        /// </summary>
+        /// <param name="store">The X509 store.</param>
+        /// <param name="selectorFunc">Filter to apply to the certificate collection.</param>
+        /// <returns>Collection of certificates.</returns>
+        public static IEnumerable<X509Certificate2> GetCertificateCollectionViaSelector(X509Store store, Func<X509Certificate2, bool> selectorFunc)
+        {
+            if (store == null)
+            {
+                throw new ArgumentNullException(nameof(store));
+            }
+
+            if (selectorFunc == null)
+            {
+                throw new ArgumentNullException(nameof(selectorFunc));
+            }
+
+            foreach (var storeCertificate in store.Certificates)
+            {
+                if (!selectorFunc(storeCertificate))
+                {
+                    continue;
+                }
+
+                yield return storeCertificate;
+            }
+        }
+    }
+}
