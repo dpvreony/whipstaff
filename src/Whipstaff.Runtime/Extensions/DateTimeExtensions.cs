637b29d5faadb06ad4e9905427032c990b8864a3
David Vreony <dpvreony@users.noreply.github.com>
2022-11-04 09:02:11 +0000

    initial extension bits

---
+++ src/Whipstaff.Runtime/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) 2022 DHGMS Solutions and Contributors. All rights reserved.
+// This file is licensed to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System;
+
+namespace Whipstaff.Runtime.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static string ToRfc3339String(this DateTime dateTime)
+        {
+            // https://stackoverflow.com/questions/17017/how-do-i-parse-and-convert-a-datetime-to-the-rfc-3339-date-time-format
+            return dateTime.ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ss") + "Z";
+        }
+    }
+}
---
+++ src/Whipstaff.Runtime/Extensions/StreamExtensions.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (c) 2022 DHGMS Solutions and Contributors. All rights reserved.
+// This file is licensed to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Threading.Tasks;
+
+namespace Whipstaff.Runtime.Extensions
+{
+    public static class StreamExtensions
+    {
+        public static async Task<TResult> GetPocoResponseFromJsonViaDataContractAsync<TResult>(this Stream stream)
+        {
+            return await Task.FromResult(GetPocoResponseFromJsonViaDataContract<TResult>(stream)).ConfigureAwait(false);
+        }
+
+        public static async Task<TResult> GetPocoResponseFromJsonViaDataContractAsync<TResult>(this Stream stream, DataContractJsonSerializer dataContractJsonSerializer)
+        {
+            return await Task.FromResult(GetPocoResponseFromJsonViaDataContract<TResult>(stream, dataContractJsonSerializer)).ConfigureAwait(false);
+        }
+
+        public static TResult GetPocoResponseFromJsonViaDataContract<TResult>(this Stream stream)
+        {
+            var serializer = new DataContractJsonSerializer(typeof(TResult));
+            return (TResult)serializer.ReadObject(stream);
+        }
+
+        public static TResult GetPocoResponseFromJsonViaDataContract<TResult>(this Stream stream, DataContractJsonSerializer dataContractJsonSerializer)
+        {
+            if (dataContractJsonSerializer == null)
+            {
+                throw new ArgumentNullException(nameof(dataContractJsonSerializer));
+            }
+
+            var serializerType = dataContractJsonSerializer.GetType();
+            if (serializerType == typeof(TResult))
+            {
+                throw new ArgumentException($"Type on serializer is incorrect. Expected: {typeof(TResult)}, Got: {serializerType}", nameof(dataContractJsonSerializer));
+            }
+
+            return (TResult)dataContractJsonSerializer.ReadObject(stream);
+        }
+    }
+}
