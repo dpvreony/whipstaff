64af31f20fedba97595f56446f14c98a67442d6e
David Vreony <dpvreony@users.noreply.github.com>
2022-11-12 12:33:32 +0000

    add entity as type interfaces (#1237)

---
+++ src/Whipstaff.Core/Entities/IEntityAsString.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) 2022 DHGMS Solutions and Contributors. All rights reserved.
+// This file is licensed to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+namespace Whipstaff.Core.Entities
+{
+    /// <inheritdoc />
+    public interface IEntityAsString : IEntityAsType<string>
+    {
+    }
+}
