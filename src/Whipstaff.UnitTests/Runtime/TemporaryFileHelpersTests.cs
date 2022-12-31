97a5446cfdbede9b8b832eac690f5d86b8c1b3aa
David Vreony <dpvreony@users.noreply.github.com>
2022-12-31 21:40:03 +0000

    add basics of unit tests

---
+++ src/Whipstaff.UnitTests/Runtime/TemporaryFileHelpersTests.cs
@@ -0,0 +1,64 @@
+﻿// Copyright (c) 2022 DHGMS Solutions and Contributors. All rights reserved.
+// This file is licensed to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System;
+using Whipstaff.Runtime.FileProcessing;
+using Xunit;
+
+namespace Whipstaff.UnitTests.Runtime
+{
+    /// <summary>
+    /// Unit Tests for <see cref="TemporaryFileHelpers"/>.
+    /// </summary>
+    public static class TemporaryFileHelpersTests
+    {
+        /// <summary>
+        /// Unit Tests for <see cref="TemporaryFileHelpers.WithTempFile"/>.
+        /// </summary>
+        public sealed class WithTempFileMethod : NetTestRegimentation.ITestMethodWithNullableParameters<byte[], string, Action<string>>
+        {
+            /// <inheritdoc />
+            [Theory]
+            [ClassData(typeof(Whipstaff.UnitTests.TestSources.Runtime.TemporaryFileHelpersTests.WithTempFileMethod.ThrowsArgumentNullExceptionTestSource))]
+            public void ThrowsArgumentNullException(
+                byte[] arg1,
+                string arg2,
+                Action<string> arg3,
+                string expectedParameterNameForException)
+            {
+                var exception = Assert.Throws<ArgumentNullException>(() => TemporaryFileHelpers.WithTempFile(
+                    arg1,
+                    arg2,
+                    arg3,
+                    false));
+
+                Assert.Equal(expectedParameterNameForException, exception.ParamName);
+            }
+        }
+
+        /// <summary>
+        /// Unit Tests for <see cref="TemporaryFileHelpers.WithTempFile{TResult}"/>.
+        /// </summary>
+        public sealed class WithTempFileT1Method : NetTestRegimentation.ITestMethodWithNullableParameters<byte[], string, Func<string, int>>
+        {
+            /// <inheritdoc />
+            [Theory]
+            [ClassData(typeof(Whipstaff.UnitTests.TestSources.Runtime.TemporaryFileHelpersTests.WithTempFileT1Method.ThrowsArgumentNullExceptionTestSource))]
+            public void ThrowsArgumentNullException(
+                byte[] arg1,
+                string arg2,
+                Func<string, int> arg3,
+                string expectedParameterNameForException)
+            {
+                var exception = Assert.Throws<ArgumentNullException>(() => TemporaryFileHelpers.WithTempFile(
+                    arg1,
+                    arg2,
+                    arg3,
+                    false));
+
+                Assert.Equal(expectedParameterNameForException, exception.ParamName);
+            }
+        }
+    }
+}
---
+++ src/Whipstaff.UnitTests/TestSources/Runtime/TemporaryFileHelpersTests/WithTempFileT1Method/ThrowsArgumentNullExceptionTestSource.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) 2022 DHGMS Solutions and Contributors. All rights reserved.
+// This file is licensed to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System;
+using Xunit;
+
+namespace Whipstaff.UnitTests.TestSources.Runtime.TemporaryFileHelpersTests.WithTempFileT1Method
+{
+    /// <summary>
+    /// Test Source for <see cref="Whipstaff.UnitTests.Runtime.TemporaryFileHelpersTests.WithTempFileT1Method.ThrowsArgumentNullException"/>.
+    /// </summary>
+    public sealed class ThrowsArgumentNullExceptionTestSource : TheoryData<byte[]?, string?, Func<string, int>?, string?>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ThrowsArgumentNullExceptionTestSource"/> class.
+        /// </summary>
+        public ThrowsArgumentNullExceptionTestSource()
+        {
+            var arg1 = new byte[] { 1 };
+            const string arg2 = "arg2";
+            Func<string, int> arg3 = _ => 1;
+            Add(null, arg2, arg3, "fileAsBytes");
+            Add(arg1, null, arg3, "fileExtension");
+            Add(arg1, arg2, null, "fileAction");
+        }
+    }
+}
