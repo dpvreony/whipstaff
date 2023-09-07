19e67360741e6ab16318c75d9bb8cb0d848f68ff
David Vreony <dpvreony@users.noreply.github.com>
2023-09-07 17:19:16 +0100

    log message actions wrapper concept

---
+++ src/Whipstaff.Core/Logging/ILogMessageActions.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) 2022 DHGMS Solutions and Contributors. All rights reserved.
+// This file is licensed to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+namespace Whipstaff.Core.Logging
+{
+    /// <summary>
+    /// Interface for identifying a class as a log message actions host.
+    /// There is no implementation as the intention is a marker interface for Nucleotide Source Code Generation.
+    /// </summary>
+    /// <typeparam name="TCategoryName">The type whose name is used for the logger category name.</typeparam>
+    public interface ILogMessageActions<out TCategoryName>
+    {
+    }
+}
