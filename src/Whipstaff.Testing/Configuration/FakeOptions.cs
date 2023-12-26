3c78d95357dbb529dd6b8512176856af936ec3b7
David Vreony <dpvreony@users.noreply.github.com>
2023-12-26 13:04:57 +0000

    prep of unit tests

---
+++ src/Whipstaff.Testing/Configuration/FakeOptions.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) 2022 DHGMS Solutions and Contributors. All rights reserved.
+// This file is licensed to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System.ComponentModel.DataAnnotations;
+
+namespace Whipstaff.Testing.Configuration
+{
+    /// <summary>
+    /// Fake options class for testing.
+    /// </summary>
+    public sealed class FakeOptions
+    {
+        /// <summary>
+        /// Gets or sets the fake string.
+        /// </summary>
+        [Required]
+        [MinLength(1)]
+        [MaxLength(10)]
+        public string? FakeString { get; set; }
+    }
+}
---
+++ src/Whipstaff.UnitTests/Core/Configuration/ServiceCollectionExtensionsTests.cs
@@ -0,0 +1,77 @@
+﻿// Copyright (c) 2022 DHGMS Solutions and Contributors. All rights reserved.
+// This file is licensed to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System;
+using Microsoft.Extensions.Configuration;
+using NetTestRegimentation;
+using Whipstaff.Core.Configuration;
+using Whipstaff.Testing.Configuration;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Whipstaff.UnitTests.Core.Configuration
+{
+    /// <summary>
+    /// Unit tests for <see cref="ServiceCollectionExtensions"/>.
+    /// </summary>
+    public static class ServiceCollectionExtensionsTests
+    {
+        /// <summary>
+        /// Unit Tests for the <see cref="ServiceCollectionExtensions.AddStrictConfigurationBinding{TOptions}(Microsoft.Extensions.DependencyInjection.IServiceCollection,string,System.Func{TOptions,bool})"/> method.
+        /// </summary>
+        public sealed class AddStrictConfigurationBindingT1Method
+            : Foundatio.Xunit.TestWithLoggingBase,
+                ITestMethodWithNullableParameters<string, Func<FakeOptions, bool>>
+        {
+            /// <summary>
+            /// Initializes a new instance of the <see cref="AddStrictConfigurationBindingT1Method"/> class.
+            /// </summary>
+            /// <param name="output">XUnit test output helper instance.</param>
+            public AddStrictConfigurationBindingT1Method(ITestOutputHelper output)
+                : base(output)
+            {
+            }
+
+            /// <inheritdoc/>
+            public void ThrowsArgumentNullException(
+                string arg1,
+                Func<FakeOptions, bool> arg2,
+                string expectedParameterNameForException)
+            {
+                var services = new Microsoft.Extensions.DependencyInjection.ServiceCollection();
+
+                _ = Assert.Throws<ArgumentNullException>(
+                    expectedParameterNameForException,
+                    () => services.AddStrictConfigurationBinding(arg1, arg2));
+            }
+        }
+
+        /// <summary>
+        /// Unit Tests for the <see cref="ServiceCollectionExtensions.AddStrictConfigurationBinding{TOptions,TOptionsValidator}(Microsoft.Extensions.DependencyInjection.IServiceCollection,Microsoft.Extensions.Configuration.IConfiguration,string)"/> method.
+        /// </summary>
+        public sealed class AddStrictConfigurationBindingT2Method
+            : Foundatio.Xunit.TestWithLoggingBase,
+                ITestMethodWithNullableParameters<IConfiguration, string>
+        {
+            /// <summary>
+            /// Initializes a new instance of the <see cref="AddStrictConfigurationBindingT2Method"/> class.
+            /// </summary>
+            /// <param name="output">XUnit test output helper instance.</param>
+            public AddStrictConfigurationBindingT2Method(ITestOutputHelper output)
+                : base(output)
+            {
+            }
+
+            /// <inheritdoc/>
+            public void ThrowsArgumentNullException(IConfiguration arg1, string arg2, string expectedParameterNameForException)
+            {
+                var services = new Microsoft.Extensions.DependencyInjection.ServiceCollection();
+
+                _ = Assert.Throws<ArgumentNullException>(
+                    expectedParameterNameForException,
+                    () => services.AddStrictConfigurationBinding<FakeOptions, FakeOptionsValidator>(arg1, arg2));
+            }
+        }
+    }
+}
