17d383426e1e4c3d29ae353e4f6f58de349d5338
David Vreony <dpvreony@users.noreply.github.com>
2024-01-25 20:19:46 +0000

    add thread safe counters for jobs

---
+++ src/Whipstaff.Runtime/Counters/IncrementOnlyInt.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading;
+
+namespace Whipstaff.Runtime.Counters
+{
+    /// <summary>
+    /// Wrapper for an integer that can only be incremented. Useful for operation counters.
+    /// </summary>
+    public sealed class IncrementOnlyInt
+    {
+        private int _value;
+
+        /// <summary>
+        /// Gets the current value of the counter.
+        /// </summary>
+        public int Value => _value;
+
+        /// <summary>
+        /// Increments the counter by one.
+        /// </summary>
+        /// <returns>
+        /// The incremented value.
+        /// </returns>
+        public int Increment()
+        {
+            return Interlocked.Increment(ref _value);
+        }
+    }
+}
