fb537b45648127bbee55d454611087f5c4f8591e
David Vreony <dpvreony@users.noreply.github.com>
2024-02-07 23:56:52 +0000

    add extension for replacing string using a dictionary (#1634)
    * add extension for replacing string using a dictionary
    
    * Update StringExtensions.cs
    
    * logic for ms word string cleansing
    
    * add unit tests
    
    * Create BatchStringReplacementHelperTests.cs

--- src/Whipstaff.Runtime/Extensions/StringExtensions.cs
+++ src/Whipstaff.Runtime/Extensions/StringExtensions.cs
@@ -3,10 +3,16 @@
 // See the LICENSE file in the project root for full license information.
 
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
+using Whipstaff.Runtime.StringCleansing;
+
+#if ARGUMENT_NULL_EXCEPTION_SHIM
+using ArgumentNullException = Whipstaff.Runtime.Exceptions.ArgumentNullException;
+#endif
 
 namespace Whipstaff.Runtime.Extensions
 {
@@ -23,10 +29,7 @@ namespace Whipstaff.Runtime.Extensions
         /// <exception cref="ArgumentNullException">No action is provided.</exception>
         public static void ActIfNotNullOrWhiteSpace(this string? input, Action<string> action)
         {
-            if (action == null)
-            {
-                throw new ArgumentNullException(nameof(action));
-            }
+            ArgumentNullException.ThrowIfNull(action);
 
             if (string.IsNullOrWhiteSpace(input))
             {
@@ -52,28 +55,125 @@ namespace Whipstaff.Runtime.Extensions
             return instance.All(character => character.IsHexadecimal());
         }
 
+        /// <summary>
+        /// Replaces characters in a string using a dictionary.
+        /// </summary>
+        /// <param name="instance">String to check.</param>
+        /// <param name="replacements">Dictionary of replacements to carry out.</param>
+        /// <returns>Altered string.</returns>
+        public static string Replace(
+            this string instance,
+            Dictionary<char, char> replacements)
+        {
+            ArgumentNullException.ThrowIfNull(instance);
+            ArgumentNullException.ThrowIfNull(replacements);
+
+            var charArray = instance.ToCharArray();
+
+            for (var i = 0; i < charArray.Length; i++)
+            {
+                var currentChar = charArray[i];
+
+                if (replacements.TryGetValue(currentChar, out var value))
+                {
+                    charArray[i] = value;
+                }
+            }
+
+            return new string(charArray);
+        }
+
+        /// <summary>
+        /// Replaces characters in a string using a dictionary.
+        /// </summary>
+        /// <param name="instance">String to check.</param>
+        /// <param name="replacements">Dictionary of replacements to carry out.</param>
+        /// <returns>Altered string.</returns>
+        public static string Replace(
+            this string instance,
+            Dictionary<char, string> replacements)
+        {
+            ArgumentNullException.ThrowIfNull(instance);
+            ArgumentNullException.ThrowIfNull(replacements);
+
+            var linkedList = new LinkedList<char>(instance);
+            var currentNode = linkedList.First;
+            while (currentNode != null)
+            {
+                var currentChar = currentNode.Value;
+
+                if (replacements.TryGetValue(currentChar, out var value))
+                {
+                    if (value == null)
+                    {
+                        // remove the character
+                        continue;
+                    }
+
+                    switch (value.Length)
+                    {
+                        case 0:
+                            // remove the character
+                            var oldNode = currentNode;
+                            currentNode = currentNode.Next;
+                            linkedList.Remove(oldNode);
+                            continue;
+                        case 1:
+                            currentNode.Value = value[0];
+                            break;
+                        default:
+                            currentNode.Value = value[0];
+
+                            for (int i = 1; i < value.Length; i++)
+                            {
+                                currentNode = linkedList.AddAfter(currentNode, value[i]);
+                            }
+
+                            break;
+                    }
+                }
+
+                currentNode = currentNode.Next;
+            }
+
+            var stringBuilder = new StringBuilder();
+            var linkedListEnumerable = linkedList.Select(c => c).ToArray();
+            _ = stringBuilder.Append(linkedListEnumerable);
+
+            return stringBuilder.ToString();
+        }
+
+        /// <summary>
+        /// Replaces Microsoft Word smart quotes with ASCII equivalents.
+        /// </summary>
+        /// <param name="instance">The string to work on.</param>
+        /// <returns>Altered string.</returns>
+        public static string ReplaceMsWordSmartQuotesWithAscii(this string instance)
+        {
+            var replacements = StringCleansingDictionaryFactories.GetMicrosoftWordSmartQuoteReplacements();
+
+            return instance.Replace(replacements);
+        }
+
 #if NETSTANDARD2_1_OR_GREATER
         /// <summary>
         /// Removes all instances of a string.
         /// </summary>
         /// <param name="instance">The string to work on.</param>
         /// <param name="value">The string to remove.</param>
-        /// <param name="ignoreCase">Whether to ignore the case, or be case-sensitive.</param>
+        /// <param name="comparisonType">One of the enumeration values that determines how value is searched within this instance.</param>
         /// <returns>Altered string.</returns>
         public static string Remove(
             this string instance,
             string value,
-            bool ignoreCase = false)
+            StringComparison comparisonType)
         {
-            if (instance == null)
-            {
-                throw new ArgumentNullException(nameof(instance));
-            }
+            ArgumentNullException.ThrowIfNull(instance);
 
             return instance.Replace(
                 value,
                 string.Empty,
-                ignoreCase ? StringComparison.OrdinalIgnoreCase : StringComparison.Ordinal);
+                comparisonType);
         }
 #endif
 
@@ -84,10 +184,7 @@ namespace Whipstaff.Runtime.Extensions
         /// <returns>Memory stream.</returns>
         public static MemoryStream ToMemoryStream(this string instance)
         {
-            if (instance == null)
-            {
-                throw new ArgumentNullException(nameof(instance));
-            }
+            ArgumentNullException.ThrowIfNull(instance);
 
             var byteArray = Encoding.Unicode.GetBytes(instance);
             return new MemoryStream(byteArray);
@@ -104,17 +201,31 @@ namespace Whipstaff.Runtime.Extensions
         }
 
         /// <summary>
-        /// Throws an <see cref="ArgumentNullException"/> if a string is null or whitespace.
+        /// Throws a <see cref="ArgumentNullException"/> if a string is null or whitespace.
         /// </summary>
         /// <param name="argument">string to check.</param>
         /// <param name="paramName">The name of the parameter with which <paramref name="argument" /> corresponds.</param>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static void ThrowIfNullOrWhitespace(this string? argument, [CallerArgumentExpression(nameof(argument))] string? paramName = null)
+        public static void ThrowIfNullOrWhitespace(
+            this string? argument,
+            [CallerArgumentExpression(nameof(argument))] string? paramName = null)
         {
-            if (string.IsNullOrWhiteSpace(argument))
+            if (argument == null)
             {
-                throw new ArgumentNullException(paramName);
+                throw new System.ArgumentNullException(paramName);
             }
+
+            foreach (var c in argument)
+            {
+                if (!char.IsWhiteSpace(c))
+                {
+                    return;
+                }
+            }
+
+            throw new ArgumentException(
+                "input is all whitespace",
+                paramName);
         }
     }
 }
---
+++ src/Whipstaff.Runtime/StringCleansing/StringCleansingDictionaryFactories.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) 2022 DHGMS Solutions and Contributors. All rights reserved.
+// This file is licensed to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+
+namespace Whipstaff.Runtime.StringCleansing
+{
+    /// <summary>
+    /// Factories for creating <see cref="Dictionary{TKey,TValue}"/> instances for string cleansing.
+    /// </summary>
+    public static class StringCleansingDictionaryFactories
+    {
+        /// <summary>
+        /// Gets a dictionary of Microsoft Word smart quote replacements.
+        /// </summary>
+        /// <returns>Dictionary of replacements.</returns>
+        public static Dictionary<char, string> GetMicrosoftWordSmartQuoteReplacements()
+        {
+            return new Dictionary<char, string>
+            {
+                { '\u2014', "-" },
+                { '\u2015', "-" },
+                { '\u2017', "_" },
+                { '\u2018', "\'" },
+                { '\u2019', "\'" },
+                { '\u201a', "'," },
+                { '\u201b', "\'" },
+                { '\u201c', "\"" },
+                { '\u201d', "\"" },
+                { '\u201e', "\"" },
+                { '\u2032', "\'" },
+                { '\u2033', "\"" },
+                { '\u0092', "\'" },
+                { '\u2026', "..." },
+                { '\u0085', "..." },
+            };
+        }
+    }
+}
