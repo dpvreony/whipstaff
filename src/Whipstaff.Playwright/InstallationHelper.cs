b1712db33f473fe0ffaba3e620bfdd32a51dbab2
David Vreony <dpvreony@users.noreply.github.com>
2024-08-03 13:46:30 +0100

    Feature: mermaid helpers for installation and doing file based generation (#1814)
    * add a default factory for the playwright mermaid renderer
    
    * add file handler overload for getdiagram
    
    * add getdiagram text reader overload
    
    * Create InstallationHelper.cs
    
    * Create InstallationHelperTests.cs
    
    * logic to write diagram to file
    
    * more unit tests
    
    * Update PlaywrightRendererTests.cs
    
    * Update PlaywrightRendererTests.cs
    
    * add PlaywrightBrowserTypeAndChannel combination model
    
    * refactor to use PlaywrightBrowserTypeAndChannel arg

---
+++ src/Whipstaff.Playwright/InstallationHelper.cs
@@ -0,0 +1,49 @@
+﻿// Copyright (c) 2022 DHGMS Solutions and Contributors. All rights reserved.
+// This file is licensed to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System;
+using Microsoft.Playwright;
+
+namespace Whipstaff.Playwright
+{
+    /// <summary>
+    /// Helper class for installing Playwright browser. The out-of-the-box experience is a bit clunky.
+    ///
+    /// This attempts to make it a bit easier. By allowing a first run execution to install the browser.
+    ///
+    /// See:
+    /// https://github.com/microsoft/playwright-dotnet/issues/2239
+    /// https://github.com/microsoft/playwright-dotnet/issues/2286
+    ///
+    /// This also removes the need to have msbuild run the task which has been causing CodeQL failures.
+    /// </summary>
+    public sealed class InstallationHelper
+    {
+        private readonly Lazy<int> _lazyInstaller;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InstallationHelper"/> class.
+        /// </summary>
+        /// <param name="playwrightBrowserType">The browser to use as part of the process.</param>
+        public InstallationHelper(PlaywrightBrowserType playwrightBrowserType)
+        {
+            var args = playwrightBrowserType switch
+            {
+                PlaywrightBrowserType.None => new[] { "install" },
+                _ => new[] { "install", playwrightBrowserType.ToString().ToLowerInvariant() },
+            };
+
+            _lazyInstaller = new(() => Program.Main(args));
+        }
+
+        /// <summary>
+        /// Wrapper to carry out an installation a single time.
+        /// </summary>
+        /// <returns>result code from Playwright install.</returns>
+        public int InstallPlaywright()
+        {
+            return _lazyInstaller.Value;
+        }
+    }
+}
---
+++ src/Whipstaff.UnitTests/Playwright/InstallationHelperTests.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) 2022 DHGMS Solutions and Contributors. All rights reserved.
+// This file is licensed to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using Whipstaff.Playwright;
+using Xunit;
+
+namespace Whipstaff.UnitTests.Playwright
+{
+    /// <summary>
+    /// Unit Tests for <see cref="InstallationHelper"/>.
+    /// </summary>
+    public static class InstallationHelperTests
+    {
+        /// <summary>
+        /// Unit tests for the constructor.
+        /// </summary>
+        public sealed class ConstructorMethod
+        {
+            /// <summary>
+            /// Tests that the constructor returns an instance when no browser is passed.
+            /// </summary>
+            [Fact]
+            public void ReturnsInstance()
+            {
+                var instance = new InstallationHelper(PlaywrightBrowserType.None);
+                Assert.NotNull(instance);
+            }
+
+            /// <summary>
+            /// Tests that the constructor returns an instance when a specific browser is passed.
+            /// </summary>
+            [Fact]
+            public void ReturnsInstanceWithBrowser()
+            {
+                var instance = new InstallationHelper(PlaywrightBrowserType.Chromium);
+                Assert.NotNull(instance);
+            }
+        }
+    }
+}
