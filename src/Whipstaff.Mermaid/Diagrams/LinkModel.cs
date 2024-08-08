347a062bd95d8e28302b3005b6d6a27a4ba05c43
David Vreony <dpvreony@users.noreply.github.com>
2024-08-08 13:55:59 +0100

    initial diagram model concept

---
+++ src/Whipstaff.Mermaid/Diagrams/LinkModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Whipstaff.Mermaid.Diagrams
+{
+    public sealed record LinkModel
+    {
+        public LinkModel Invisble(string? text = null) => new();
+
+        public LinkModel StandardLine(string? text = null) => new();
+
+        public LinkModel DottedLine(string? text = null) => new();
+
+        public LinkModel ThickLine(string? text = null) => new();
+    }
+}
