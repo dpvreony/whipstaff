22c50c26bfd3b839089bf21c99f2b6ed66122931
David Vreony <dpvreony@users.noreply.github.com>
2024-11-08 15:51:37 +0000

    add aspire app host example

---
+++ src/Whipstaff.Example.AspireAppHost/Program.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) 2022 DHGMS Solutions and Contributors. All rights reserved.
+// This file is licensed to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using Aspire.Hosting;
+using Whipstaff.Aspire.Hosting.ZedAttackProxy;
+
+var builder = DistributedApplication.CreateBuilder(args);
+
+var apiSite = builder.AddProject<Projects.Dhgms_AspNetCoreContrib_Example_WebApiApp>("api_site")
+    .WithExternalHttpEndpoints();
+
+var mvcSite = builder.AddProject<Projects.Dhgms_AspNetCoreContrib_Example_WebMvcApp>("mvc_site")
+    .WithExternalHttpEndpoints();
+
+if (builder.ExecutionContext.IsRunMode)
+{
+    var zapApiKey = "ZAPROXY-API-SECRET";
+
+    _ = builder.AddZedAttackProxyContainerAsDaemon(
+            60080,
+            zapApiKey)
+        .WithReference(apiSite)
+        .WithReference(mvcSite);
+}
+
+await builder.Build().RunAsync();
