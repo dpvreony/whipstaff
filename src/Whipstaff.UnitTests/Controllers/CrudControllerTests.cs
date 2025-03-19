a76e8258f8d2a0db512e686ded226dc47533e93a
David Vreony <dpvreony@users.noreply.github.com>
2025-03-19 22:58:29 +0000

    more prep for rocks

--- src/Whipstaff.UnitTests/Controllers/CrudControllerTests.cs
+++ src/Whipstaff.UnitTests/Controllers/CrudControllerTests.cs
@@ -24,27 +24,6 @@ using Xunit;
 
 namespace Whipstaff.UnitTests.Controllers
 {
-    /// <summary>
-    /// Expectations for the Authorization Service.
-    /// </summary>
-    [RockPartial(typeof(IAuthorizationService), BuildType.Create)]
-#pragma warning disable SA1402 // File may only contain a single type
-    public sealed partial class IAuthorizationServiceCreateExpectations;
-
-    /// <summary>
-    /// Expectations for the Mediator.
-    /// </summary>
-    [RockPartial(typeof(IMediator), BuildType.Create)]
-    public sealed partial class IMediatorCreateExpectations;
-
-    /// <summary>
-    /// Expectations for the Mediator.
-    /// </summary>
-    /// <typeparam name="TCategoryName">The type whose name is used for the logger category name.</typeparam>
-    [RockPartial(typeof(ILogger<>), BuildType.Create)]
-    public sealed partial class ILoggerCreateExpectations<TCategoryName>;
-#pragma warning restore SA1402 // File may only contain a single type
-
 #pragma warning disable CA1034
     /// <summary>
     /// Unit Tests for the CRUD controller.
@@ -242,7 +221,7 @@ namespace Whipstaff.UnitTests.Controllers
             public async Task ShouldSucceedAsync(int addRequestDto)
             {
                 var authorizationService = MockAuthorizationServiceFactory();
-                authorizationService.Methods.AuthorizeAsync(
+                _ = authorizationService.Methods.AuthorizeAsync(
                         Arg.Any<ClaimsPrincipal>(),
                         Arg.Any<int>(),
                         Arg.Is("addPolicyName"))
@@ -251,7 +230,7 @@ namespace Whipstaff.UnitTests.Controllers
                 var logger = MockLoggerFactory();
 
                 var mediator = MockMediatorFactory();
-                _ = mediator.Methods.Send<int?>(
+                _ = mediator.Methods.Send(
                         Arg.Validate<IRequest<int?>>(query => true),
                         Arg.Any<CancellationToken>())
                     .Callback(static (a, b) =>
@@ -286,7 +265,9 @@ namespace Whipstaff.UnitTests.Controllers
                 _ = Assert.IsType<OkObjectResult>(result.Result);
             }
 
+#pragma warning disable S1172
             private static async Task<int?> MockAddMediatorHandlerAsync(IAuditableRequest<int, int?> auditableRequest, CancellationToken cancellationToken)
+#pragma warning restore S1172
             {
                 return await Task.FromResult(auditableRequest.RequestDto);
             }
@@ -334,14 +315,23 @@ namespace Whipstaff.UnitTests.Controllers
             public async Task ShouldSucceedAsync(int id)
             {
                 var authorizationService = MockAuthorizationServiceFactory();
-                _ = authorizationService.Setup(s =>
-                    s.AuthorizeAsync(It.IsAny<ClaimsPrincipal>(), It.IsAny<object>(), "deletePolicyName"))
-                    .Returns(async () => await Task.FromResult(AuthorizationResult.Success()));
+                _ = authorizationService.Methods.AuthorizeAsync(
+                        Arg.Any<ClaimsPrincipal>(),
+                        Arg.Any<object?>(),
+                        Arg.Is("deletePolicyName"))
+                    .ReturnValue(Task.FromResult(AuthorizationResult.Success()));
 
                 var logger = MockLoggerFactory();
 
                 var mediator = MockMediatorFactory();
-                _ = mediator.Setup(s => s.Send(It.IsAny<IAuditableRequest<long, long?>>(), It.IsAny<CancellationToken>())).Returns<IAuditableRequest<long, long?>, CancellationToken>(MockDeleteMediatorHandlerAsync);
+                _ = mediator.Methods.Send(
+                        Arg.Validate<IRequest<long?>>(query => true),
+                        Arg.Any<CancellationToken>())
+                    .Callback(static (a, b) =>
+                    {
+                        var fakeCrudViewQuery = a as IAuditableRequest<long, long?>;
+                        return MockDeleteMediatorHandlerAsync(fakeCrudViewQuery!, b);
+                    });
 
                 var commandFactory = MockCommandFactory();
                 var queryFactory = MockQueryFactory();
@@ -389,7 +379,15 @@ namespace Whipstaff.UnitTests.Controllers
                 var logger = MockLoggerFactory();
 
                 var mediator = MockMediatorFactory();
-                _ = mediator.Setup(s => s.Send(It.IsAny<IAuditableRequest<long, long?>>(), It.IsAny<CancellationToken>())).Returns<IAuditableRequest<long, long?>, CancellationToken>(MockDeleteMediatorHandlerAsync);
+
+                _ = mediator.Methods.Send(
+                        Arg.Validate<IRequest<long?>>(query => true),
+                        Arg.Any<CancellationToken>())
+                    .Callback(static (a, b) =>
+                    {
+                        var fakeCrudViewQuery = a as IAuditableRequest<long, long?>;
+                        return MockDeleteMediatorHandlerAsync(fakeCrudViewQuery!, b);
+                    });
 
                 var commandFactory = MockCommandFactory();
                 var queryFactory = MockQueryFactory();
@@ -417,7 +415,9 @@ namespace Whipstaff.UnitTests.Controllers
                 _ = Assert.IsType<NotFoundResult>(result.Result);
             }
 
+#pragma warning disable S1172 // Unused method parameters should be removed
             private static async Task<long?> MockDeleteMediatorHandlerAsync(IAuditableRequest<long, long?> arg1, CancellationToken arg2)
+#pragma warning restore S1172 // Unused method parameters should be removed
             {
                 return await Task.FromResult(arg1.RequestDto).ConfigureAwait(false);
             }
@@ -470,7 +470,14 @@ namespace Whipstaff.UnitTests.Controllers
                 var logger = MockLoggerFactory();
 
                 var mediator = MockMediatorFactory();
-                _ = mediator.Setup(s => s.Send(It.IsAny<FakeCrudListQuery>(), It.IsAny<CancellationToken>())).Returns<FakeCrudListQuery, CancellationToken>(MockListMediatorHandlerAsync);
+                _ = mediator.Methods.Send(
+                        Arg.Validate<IRequest<IList<int>?>>(query => true),
+                        Arg.Any<CancellationToken>())
+                    .Callback(static (a, b) =>
+                    {
+                        var fakeCrudViewQuery = a as FakeCrudListQuery;
+                        return MockListMediatorHandlerAsync(fakeCrudViewQuery!, b);
+                    });
 
                 var commandFactory = MockCommandFactory();
                 var queryFactory = MockQueryFactory();
@@ -503,7 +510,9 @@ namespace Whipstaff.UnitTests.Controllers
             }
 
 #pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+#pragma warning disable S1172 // Unused method parameters should be removed
             private static async Task<IList<int>?> MockListMediatorHandlerAsync(FakeCrudListQuery auditableRequest, CancellationToken cancellationToken)
+#pragma warning restore S1172 // Unused method parameters should be removed
 #pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
             {
                 return new List<int> { 1, 2, 3 };
@@ -553,7 +562,14 @@ namespace Whipstaff.UnitTests.Controllers
                 var logger = MockLoggerFactory();
 
                 var mediator = MockMediatorFactory();
-                _ = mediator.Setup(s => s.Send(It.IsAny<FakeCrudUpdateCommand>(), It.IsAny<CancellationToken>())).Returns<FakeCrudUpdateCommand, CancellationToken>(MockUpdateMediatorHandlerAsync);
+                _ = mediator.Methods.Send(
+                        Arg.Validate<IRequest<FakeCrudUpdateResponse?>>(query => true),
+                        Arg.Any<CancellationToken>())
+                    .Callback(static (a, b) =>
+                    {
+                        var fakeCrudViewQuery = a as FakeCrudUpdateCommand;
+                        return MockUpdateMediatorHandlerAsync(fakeCrudViewQuery!, b);
+                    });
 
                 var commandFactory = MockCommandFactory();
                 var queryFactory = MockQueryFactory();
@@ -581,7 +597,9 @@ namespace Whipstaff.UnitTests.Controllers
                 _ = Assert.IsType<OkObjectResult>(result.Result);
             }
 
+#pragma warning disable S1172 // Unused method parameters should be removed
             private static Task<FakeCrudUpdateResponse?> MockUpdateMediatorHandlerAsync(FakeCrudUpdateCommand arg1, CancellationToken arg2)
+#pragma warning restore S1172 // Unused method parameters should be removed
             {
                 return Task.FromResult<FakeCrudUpdateResponse?>(new FakeCrudUpdateResponse(arg1.RequestDto));
             }
@@ -685,7 +703,9 @@ namespace Whipstaff.UnitTests.Controllers
                 Assert.IsType(expectedResultType, result.Result);
             }
 
+#pragma warning disable S1172 // Unused method parameters should be removed
             private static Task<FakeCrudViewResponse?> MockViewMediatorHandlerAsync(FakeCrudViewQuery auditableRequest, CancellationToken cancellationToken)
+#pragma warning restore S1172 // Unused method parameters should be removed
             {
                 return Task.FromResult(auditableRequest.RequestDto == 1 ? new FakeCrudViewResponse(auditableRequest.RequestDto) : null);
             }
