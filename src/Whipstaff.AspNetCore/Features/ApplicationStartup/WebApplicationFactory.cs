9d3a18cd80bb9da6bccad583255f8d5c370b9640
David Vreony <dpvreony@users.noreply.github.com>
2025-04-05 21:36:44 +0100

    add factory override that takes webapplication options

--- src/Whipstaff.AspNetCore/Features/ApplicationStartup/WebApplicationFactory.cs
+++ src/Whipstaff.AspNetCore/Features/ApplicationStartup/WebApplicationFactory.cs
@@ -3,6 +3,7 @@
 // See the LICENSE file in the project root for full license information.
 
 using System;
+using System.Reflection.PortableExecutable;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -27,9 +28,40 @@ namespace Whipstaff.AspNetCore.Features.ApplicationStartup
             Action<WebApplicationBuilder>? builderOverrideAction)
             where TStartup : IWhipstaffWebAppStartup, new()
         {
-            var startup = new TStartup();
-
             var builder = WebApplication.CreateBuilder(args);
+
+            return SetUpHostApplicationBuilder<TStartup>(
+                builder,
+                builderOverrideAction);
+        }
+
+        /// <summary>
+        /// Gets a host builder based on the provided startup class.
+        /// </summary>
+        /// <typeparam name="TStartup">The type for the startup class. This is not an interface based on the ASP.NET core <see cref="IStartup"/>, this one provides hosting context and takes the app away from reflection whilst enforcing some design contract.</typeparam>
+        /// <param name="options">Options to set the web application up with.</param>
+        /// <param name="builderOverrideAction">Action to override the builder setup. Typically used to inject a test host etc.</param>
+        /// <returns>The host builder instance.</returns>
+        public static WebApplication GetHostApplicationBuilder<TStartup>(
+            WebApplicationOptions options,
+            Action<WebApplicationBuilder>? builderOverrideAction)
+            where TStartup : IWhipstaffWebAppStartup, new()
+        {
+            ArgumentNullException.ThrowIfNull(options);
+
+            var builder = WebApplication.CreateBuilder(options);
+
+            return SetUpHostApplicationBuilder<TStartup>(
+                builder,
+                builderOverrideAction);
+        }
+
+        private static WebApplication SetUpHostApplicationBuilder<TStartup>(
+            WebApplicationBuilder builder,
+            Action<WebApplicationBuilder>? builderOverrideAction)
+            where TStartup : IWhipstaffWebAppStartup, new()
+        {
+            var startup = new TStartup();
 
             startup.ConfigureAspireServiceDefaults(builder);
 
@@ -51,7 +83,6 @@ namespace Whipstaff.AspNetCore.Features.ApplicationStartup
             var appBuilder = builder.Build();
 
             startup.ConfigureWebApplication(appBuilder);
-
             return appBuilder;
         }
     }
