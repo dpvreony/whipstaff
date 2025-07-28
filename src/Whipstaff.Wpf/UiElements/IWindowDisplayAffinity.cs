b2e87199fd3485a34b2ef2c0a9c32dd8069b0c8c
David Vreony <dpvreony@users.noreply.github.com>
2025-07-28 17:11:28 +0100

    initial logic to bind setaffinity on the window

---
+++ src/Whipstaff.Wpf/UiElements/IWindowDisplayAffinity.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) 2022 DHGMS Solutions and Contributors. All rights reserved.
+// This file is licensed to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using ReactiveUI;
+using Windows.Win32.UI.WindowsAndMessaging;
+
+namespace Whipstaff.Wpf.UiElements
+{
+    /// <summary>
+    /// Represents a view model that can set the display affinity for a window.
+    /// </summary>
+    public interface IWindowDisplayAffinity
+    {
+        /// <summary>
+        /// Gets or sets the display affinity for the window.
+        /// </summary>
+        /// <remarks>
+        /// <para>
+        /// The display affinity setting specifies where the content of the window can be displayed.
+        /// </para>
+        /// <para>
+        /// See <see href="https://learn.microsoft.com/windows/win32/api/winuser/nf-winuser-setwindowdisplayaffinity#parameters">SetWindowDisplayAffinity Parameters</see> for more information.
+        /// </para>
+        /// </remarks>
+        global::Windows.Win32.UI.WindowsAndMessaging.WINDOW_DISPLAY_AFFINITY WindowDisplayAffinity { get; set; }
+    }
+}
--- src/Whipstaff.Wpf/UiElements/WindowExtensions.cs
+++ src/Whipstaff.Wpf/UiElements/WindowExtensions.cs
@@ -3,8 +3,12 @@
 // See the LICENSE file in the project root for full license information.
 
 using System;
+using System.Reactive.Linq;
 using System.Windows;
 using ReactiveMarbles.ObservableEvents;
+using ReactiveUI;
+using Splat;
+using Whipstaff.Wpf.Extensions;
 using Whipstaff.Wpf.Input;
 
 namespace Whipstaff.Wpf.UiElements
@@ -14,6 +18,33 @@ namespace Whipstaff.Wpf.UiElements
     /// </summary>
     public static class WindowExtensions
     {
+        /// <summary>
+        /// Sets up the Window Display Affinity for a Window that implements <see cref="IViewFor{TViewModel}"/> and <see cref="IWindowDisplayAffinity"/>.
+        /// </summary>
+        /// <typeparam name="TWindow">The type for the window.</typeparam>
+        /// <typeparam name="TViewModel">The type for the view model.</typeparam>
+        /// <param name="window">Parent window.</param>
+        /// <returns><see cref="System.IDisposable" /> object used to unsubscribe from the observable sequence.</returns>
+        public static IDisposable BindSetWindowDisplayAffinityInteraction<TWindow, TViewModel>(
+            this TWindow window)
+            where TWindow : Window, IViewFor<TViewModel>, IEnableLogger
+            where TViewModel : class, IWindowDisplayAffinity
+        {
+            return window.WhenAny(
+                vw => vw.ViewModel!.WindowDisplayAffinity,
+                change => change)
+                .Select(change => change.Value)
+                .Subscribe(affinity =>
+                {
+                    var affinityResult = window.SetWindowDisplayAffinity(affinity);
+                    if (!affinityResult)
+                    {
+                        window.Log()
+                            .Error($"Failed to set the display affinity for the window. Ensure that the window is a top-level window and that the display affinity is valid: {affinity}.");
+                    }
+                });
+        }
+
         /// <summary>
         /// Hooks up the Keyboard Copy Command for use with a Window. Handy for error dialogs.
         /// </summary>
