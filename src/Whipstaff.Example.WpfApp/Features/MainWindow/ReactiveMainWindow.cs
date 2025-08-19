1935bfec12b5f239236eb7b829e74d31d0d67124
David Vreony <dpvreony@users.noreply.github.com>
2025-08-19 23:38:57 +0100

    Feature : win32 window affinity flags for wpf (#2404)
    * remove old pinvoke
    
    * wpf helper for set window display affinity
    
    * initial logic to bind setaffinity on the window
    
    * rework to use an actual interaction
    
    * prep for example wpf app
    
    * prep for wpf example
    
    * Update global.json
    
    * Update codeql-analysis.yml

---
+++ src/Whipstaff.Example.WpfApp/Features/MainWindow/ReactiveMainWindow.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) 2022 DHGMS Solutions and Contributors. All rights reserved.
+// This file is licensed to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using ReactiveUI;
+
+namespace Whipstaff.Example.WpfApp.Features.MainWindow
+{
+    /// <summary>
+    /// Represents a Reactive Main Window.
+    /// </summary>
+    public class ReactiveMainWindow : ReactiveWindow<IMainViewModel>
+    {
+    }
+}
--- src/Whipstaff.Wpf/UiElements/WindowExtensions.cs
+++ src/Whipstaff.Wpf/UiElements/WindowExtensions.cs
@@ -3,9 +3,15 @@
 // See the LICENSE file in the project root for full license information.
 
 using System;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
 using System.Windows;
 using ReactiveMarbles.ObservableEvents;
+using ReactiveUI;
+using Splat;
+using Whipstaff.Wpf.Extensions;
 using Whipstaff.Wpf.Input;
+using Whipstaff.Wpf.InteractionFlows;
 
 namespace Whipstaff.Wpf.UiElements
 {
@@ -14,6 +20,29 @@ namespace Whipstaff.Wpf.UiElements
     /// </summary>
     public static class WindowExtensions
     {
+        /// <summary>
+        /// Sets up the Window Display Affinity for a Window that implements <see cref="IViewFor{TViewModel}"/> and <see cref="IWindowDisplayAffinity"/>.
+        /// </summary>
+        /// <typeparam name="TWindow">The type for the window.</typeparam>
+        /// <typeparam name="TViewModel">The type for the view model.</typeparam>
+        /// <param name="window">Parent window.</param>
+        /// <returns><see cref="System.IDisposable" /> object used to unsubscribe from the observable sequence.</returns>
+        public static IDisposable BindSetWindowDisplayAffinityInteraction<TWindow, TViewModel>(
+            this TWindow window)
+            where TWindow : Window, IViewFor<TViewModel>
+            where TViewModel : class, IWindowDisplayAffinity
+        {
+            ArgumentNullException.ThrowIfNull(window);
+
+            if (window.ViewModel == null)
+            {
+                throw new InvalidOperationException("The ViewModel property must be set before calling BindSetWindowDisplayAffinityInteraction.");
+            }
+
+            return window.ViewModel.WindowDisplayAffinity.RegisterHandlerToOutputFunc(affinity =>
+                Task.FromResult(window.SetWindowDisplayAffinity(affinity)));
+        }
+
         /// <summary>
         /// Hooks up the Keyboard Copy Command for use with a Window. Handy for error dialogs.
         /// </summary>
