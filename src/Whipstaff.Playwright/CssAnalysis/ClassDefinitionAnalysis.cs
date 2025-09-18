8baf7103a805281814c5a708e268e78690633e67
David Vreony <dpvreony@users.noreply.github.com>
2025-09-18 20:00:55 +0100

    add header

--- src/Whipstaff.Playwright/CssAnalysis/ClassDefinitionAnalysis.cs
+++ src/Whipstaff.Playwright/CssAnalysis/ClassDefinitionAnalysis.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿// Copyright (c) 2022 DHGMS Solutions and Contributors. All rights reserved.
+// This file is licensed to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System;
 using System.Collections.Generic;
 
 namespace Whipstaff.Playwright.CssAnalysis
