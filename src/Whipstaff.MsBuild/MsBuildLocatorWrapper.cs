edd4a24c0b38f869638fd25fabf5a43a724a7dde
David Vreony <dpvreony@users.noreply.github.com>
2025-10-18 21:47:36 +0100

    Add copyright and license comments to MsBuildLocatorWrapper
    Added copyright and license information to the file.

--- src/Whipstaff.MsBuild/MsBuildLocatorWrapper.cs
+++ src/Whipstaff.MsBuild/MsBuildLocatorWrapper.cs
@@ -1,3 +1,7 @@
+// Copyright (c) 2022 DHGMS Solutions and Contributors. All rights reserved.
+// This file is licensed to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
 using System;
 using System.Linq;
 using Microsoft.Build.Locator;
