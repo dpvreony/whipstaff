13ca0dd6da2750e42d064999bb027a975ad904fb
David Vreony <dpvreony@users.noreply.github.com>
2025-10-18 21:46:56 +0100

    Add solution file extensions

---
+++ src/Whipstaff.MsBuild/SolutionFileExtensions.cs
@@ -0,0 +1,26 @@
+// Copyright (c) 2022 DHGMS Solutions and Contributors. All rights reserved.
+// This file is licensed to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Build.Construction;
+
+namespace Whipstaff.MsBuild
+{
+    /// <summary>
+    /// Extension methods for <see cref="SolutionFile"/>.
+    /// </summary>
+    public static class SolutionFileExtensions
+    {
+        /// <summary>
+        /// Gets known MSBuild projects from the solution.
+        /// </summary>
+        /// <param name="solutionFile">The solution to pull projects from.</param>
+        /// <returns>Enumerable of MSBuild projects.</returns>
+        public static IEnumerable<ProjectInSolution> GetKnownMsBuildProjects(this SolutionFile solutionFile)
+        {
+            return solutionFile.ProjectsInOrder.Where(x => x.ProjectType == SolutionProjectType.KnownToBeMSBuildFormat);
+        }
+    }
+}
