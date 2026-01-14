073738e06a06958b4c18e3ec8239046c4370e7d2
David Vreony <dpvreony@users.noreply.github.com>
2026-01-14 18:09:41 +0000

    housekeeping: add basic benchmark for mermaid (#2667)
    * add basic benchmark for mermaid
    
    * suppress bunch of benchmark build errors
    
    * Update Directory.Build.props
    
    * Update Directory.Build.props

---
+++ src/Whipstaff.Benchmarks/Mermaid/Playwright/PlaywrightRendererBrowserInstanceBenchmarks.cs
@@ -0,0 +1,68 @@
+﻿// Copyright (c) 2022 DHGMS Solutions and Contributors. All rights reserved.
+// This file is licensed to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using Microsoft.Extensions.Logging.Abstractions;
+using Whipstaff.Mermaid.Playwright;
+using Whipstaff.Playwright;
+
+namespace Whipstaff.Benchmarks.Mermaid.Playwright
+{
+    /// <summary>
+    /// Benchmarks for <see cref="PlaywrightRendererBrowserInstance"/>.
+    /// </summary>
+    public class PlaywrightRendererBrowserInstanceBenchmarks
+    {
+        private const string Markdown = """
+                                        graph TD;
+                                        A-->B;
+                                        A-->C;
+                                        B-->D;
+                                        C-->D;
+                                        """;
+
+#pragma warning disable S1450 // Private fields only used as local variables in methods should become local variables
+        private PlaywrightRenderer? _playwrightRenderer;
+#pragma warning restore S1450 // Private fields only used as local variables in methods should become local variables
+        private PlaywrightRendererBrowserInstance? _browserInstance;
+
+        /// <summary>
+        /// Global setup for benchmarks.
+        /// </summary>
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+#pragma warning disable CA2000 // Dispose objects before losing scope
+            _playwrightRenderer = PlaywrightRenderer.Default(new NullLoggerFactory());
+#pragma warning restore CA2000 // Dispose objects before losing scope
+#pragma warning disable VSTHRD002 // Avoid problematic synchronous waits
+            _browserInstance = _playwrightRenderer
+                .GetBrowserSessionAsync(PlaywrightBrowserTypeAndChannel.ChromiumDefault()).Result;
+#pragma warning restore VSTHRD002 // Avoid problematic synchronous waits
+        }
+
+        /// <summary>
+        /// Global cleanup for benchmarks.
+        /// </summary>
+        [GlobalCleanup]
+        public void GlobalCleanup()
+        {
+            _browserInstance?.Dispose();
+        }
+
+        /// <summary>
+        /// Benchmark for rendering a simple diagram.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        [Benchmark]
+        public async Task GetDiagramAsync()
+        {
+            _ = await _browserInstance!.GetDiagramAsync(Markdown).ConfigureAwait(false);
+        }
+    }
+}
---
+++ src/Whipstaff.Benchmarks/Program.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) 2022 DHGMS Solutions and Contributors. All rights reserved.
+// This file is licensed to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+
+namespace Whipstaff.Benchmarks
+{
+    /// <summary>
+    /// Represents the application entry.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// Program entry point.
+        /// </summary>
+        public static void Main()
+        {
+            // This is a workaround for https://github.com/dotnet/BenchmarkDotNet/issues/2719
+            // where you can't pass semicolons in the NoWarn property via MSBuildArgument.
+            var noWarnArgs = "\"/p:NoWarn=$(NoWarn),CA1515,GR0027,GR0051,QA0002\"";
+            noWarnArgs = noWarnArgs.Replace(",", "%3B", StringComparison.Ordinal);
+
+            // Get the solution directory relative to the assembly location
+            var assemblyLocation = Assembly.GetExecutingAssembly().Location;
+#pragma warning disable IO0006 // Replace Path class with IFileSystem.Path for improved testability
+            var assemblyDirectory = Path.GetDirectoryName(assemblyLocation)!;
+            var solutionDirectory = Path.GetFullPath(Path.Combine(assemblyDirectory, "..", "..", "..", ".."));
+#pragma warning restore IO0006 // Replace Path class with IFileSystem.Path for improved testability
+
+            var job = Job.Default
+                .WithArguments(new List<Argument>
+                {
+                    new MsBuildArgument(noWarnArgs),
+                    new MsBuildArgument("/p:SolutionName=Whipstaff"),
+                    new MsBuildArgument($"/p:SolutionDir={solutionDirectory}\\")
+                });
+
+            _ = BenchmarkRunner.Run(
+                typeof(Program).Assembly,
+                DefaultConfig.Instance.AddJob(job));
+        }
+    }
+}
