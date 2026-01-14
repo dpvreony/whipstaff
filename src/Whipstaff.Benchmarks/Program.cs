28a7f769630faa3197219d229a7cf46c4f507789
David Vreony <dpvreony@users.noreply.github.com>
2026-01-14 16:00:54 +0000

    suppress bunch of benchmark build errors

--- src/Whipstaff.Benchmarks/Program.cs
+++ src/Whipstaff.Benchmarks/Program.cs
@@ -4,6 +4,8 @@
 
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
 using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Jobs;
 using BenchmarkDotNet.Running;
@@ -22,11 +24,23 @@ namespace Whipstaff.Benchmarks
         {
             // This is a workaround for https://github.com/dotnet/BenchmarkDotNet/issues/2719
             // where you can't pass semicolons in the NoWarn property via MSBuildArgument.
-            var noWarnArgs = "\"/p:NoWarn=$(NoWarn),QA0002,GR0027,GR0051\"";
+            var noWarnArgs = "\"/p:NoWarn=$(NoWarn),CA1515,GR0027,GR0051,QA0002\"";
             noWarnArgs = noWarnArgs.Replace(",", "%3B", StringComparison.Ordinal);
 
+            // Get the solution directory relative to the assembly location
+            var assemblyLocation = Assembly.GetExecutingAssembly().Location;
+#pragma warning disable IO0006 // Replace Path class with IFileSystem.Path for improved testability
+            var assemblyDirectory = Path.GetDirectoryName(assemblyLocation)!;
+            var solutionDirectory = Path.GetFullPath(Path.Combine(assemblyDirectory, "..", "..", "..", ".."));
+#pragma warning restore IO0006 // Replace Path class with IFileSystem.Path for improved testability
+
             var job = Job.Default
-                .WithArguments(new List<Argument> { new MsBuildArgument(noWarnArgs) });
+                .WithArguments(new List<Argument>
+                {
+                    new MsBuildArgument(noWarnArgs),
+                    new MsBuildArgument("/p:SolutionName=Whipstaff"),
+                    new MsBuildArgument($"/p:SolutionDir={solutionDirectory}\\")
+                });
 
             _ = BenchmarkRunner.Run(
                 typeof(Program).Assembly,
