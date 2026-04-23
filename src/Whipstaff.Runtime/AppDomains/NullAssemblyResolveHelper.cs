2e885f4506fab5a101c9699bdb6c821a30fc5464
David Vreony <dpvreony@users.noreply.github.com>
2026-04-23 23:47:13 +0100

    Feature: add wpf culture overriding (#2839)
    * Create CultureHelpers.cs
    
    * add culture overriding
    
    * update wpf sample app to actually use the bootstrap

---
+++ src/Whipstaff.Runtime/AppDomains/NullAssemblyResolveHelper.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) 2022 DHGMS Solutions and Contributors. All rights reserved.
+// This file is licensed to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System;
+using System.Reflection;
+
+namespace Whipstaff.Runtime.AppDomains
+{
+    /// <summary>
+    /// Null implementation of <see cref="IAssemblyResolveHelper"/>. This can be used when no assembly resolution is required, but an instance of <see cref="IAssemblyResolveHelper"/> is required for construction of an object.
+    /// </summary>
+    public sealed class NullAssemblyResolveHelper : IAssemblyResolveHelper
+    {
+        /// <inheritdoc/>
+        public Assembly? OnAssemblyResolve(object? sender, ResolveEventArgs args) => null;
+    }
+}
