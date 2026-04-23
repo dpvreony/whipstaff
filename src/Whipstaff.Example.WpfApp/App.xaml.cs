578fc54a58f28e6f32d10a71a860e02c154ee13f
David Vreony <dpvreony@users.noreply.github.com>
2026-04-23 22:34:06 +0100

    update wpf sample app to actually use the bootstrap

--- src/Whipstaff.Example.WpfApp/App.xaml.cs
+++ src/Whipstaff.Example.WpfApp/App.xaml.cs
@@ -3,20 +3,56 @@
 // See the LICENSE file in the project root for full license information.
 
 using System.Windows;
+using Whipstaff.Example.WpfApp.Features.Bootstrapping;
 using Whipstaff.Example.WpfApp.Features.MainWindow;
+using Whipstaff.ReactiveUI.Bootstrap;
+using Whipstaff.Runtime.AppDomains;
+using Whipstaff.Wpf;
 
 namespace Whipstaff.Example.WpfApp
 {
     /// <summary>
     /// Interaction logic for App.
     /// </summary>
-    internal sealed partial class App : Application
+    internal sealed partial class App
     {
-        /// <inheritdoc/>
-        protected override void OnStartup(StartupEventArgs e)
+#pragma warning disable GR0027 // Constructor should have a logging framework instance as the final parameter.
+        public App()
+            : base(new NullAssemblyResolveHelper())
         {
-            base.OnStartup(e);
+        }
+#pragma warning restore GR0027 // Constructor should have a logging framework instance as the final parameter.
 
+        /// <inheritdoc/>
+        protected override string? GetCultureName()
+        {
+            return "en-GB";
+        }
+
+        /// <inheritdoc/>
+        protected override void DoSplatDependencyInjectionInitialization()
+        {
+        }
+
+        /// <inheritdoc/>
+        protected override IReactiveUIBuilderRegistration GetReactiveUIRegistrations()
+        {
+            return new SampleContractModelBasedRegistration();
+        }
+
+        /// <inheritdoc/>
+        protected override void DoApplicationPerformanceMonitoringInitialization()
+        {
+        }
+
+        /// <inheritdoc/>
+        protected override void DoLoggingInitialization()
+        {
+        }
+
+        /// <inheritdoc/>
+        protected override void OnApplicationStartup(StartupEventArgs startupEventArgs)
+        {
             var view = new MainWindow();
             view.ViewModel = new MainViewModel();
 
---
+++ src/Whipstaff.Example.WpfApp/Features/Bootstrapping/SampleContractModelBasedRegistration.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) 2022 DHGMS Solutions and Contributors. All rights reserved.
+// This file is licensed to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using Whipstaff.ReactiveUI.Bootstrap;
+using Whipstaff.ReactiveUI.Bootstrap.ContractBased;
+
+namespace Whipstaff.Example.WpfApp.Features.Bootstrapping
+{
+    /// <summary>
+    /// Sample contract model based registration for the example application.
+    /// </summary>
+    internal sealed class SampleContractModelBasedRegistration : AbstractContractModelBasedRegistration
+    {
+        /// <inheritdoc/>
+        protected override IList<IConstantViewModelRegistration> ConstantViewModelRegistrations => [];
+
+        /// <inheritdoc/>
+        protected override IList<ISingletonViewRegistration> SingletonViewRegistrations => [];
+
+        /// <inheritdoc/>
+        protected override IList<ISingletonViewModelRegistration> SingletonViewModelRegistrations => [];
+
+        /// <inheritdoc/>
+        protected override IList<IViewRegistration> ViewRegistrations => [];
+
+        /// <inheritdoc/>
+        protected override IList<IViewModelRegistration> ViewModelRegistrations => [];
+    }
+}
