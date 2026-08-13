510ff4db82f4d3cc04dbb707fdd81f0b5ca41785
David Vreony <dpvreony@users.noreply.github.com>
2026-08-13 17:03:47 +0100

    Create PlaywrightBrowserTypeAndChannelCommandLineArgument.cs

---
+++ src/Whipstaff.DocFxGen.DotNetTool/CommandLine/PlaywrightBrowserTypeAndChannelCommandLineArgument.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Whipstaff.DocFxGen.DotNetTool.CommandLine
+{
+    /// <summary>
+    /// enum representation of the supported Playwright browser types and channels.
+    /// </summary>
+    public enum PlaywrightBrowserTypeAndChannelCommandLineArgument
+    {
+        None,
+        ChromiumDefault,
+        Chrome,
+        ChromeBeta,
+        ChromiumCustom,
+        MsEdge,
+        MsEdgeBeta,
+        MsEdgeDev,
+        Firefox,
+        Webkit
+    }
+}
